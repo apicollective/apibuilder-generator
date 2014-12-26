5f2d96376eb85593582edc6224368f39091caecf
Michael Bryzek <michael@gilt.com>
2014-12-25 18:45:05 -0800

    Stand-alone class to compute default paths

---
+++ api/app/models/Paths.scala
@@ -0,0 +1,21 @@
+package models
+
+import lib.Text
+import com.gilt.apidocgenerator.models.{Operation, Resource}
+
+object Paths {
+
+  def resource(modelName: String, modelPlural: Option[String] = None, resource: Resource): String = {
+    println("modelName: " + modelName)
+    resource.path.getOrElse {
+      "/" + modelPlural.getOrElse(Text.pluralize(modelName))
+    }
+  }
+
+  def operation(modelName: String, modelPlural: Option[String] = None, resource: Resource, operation: Operation): String = {
+    operation.path.getOrElse {
+      Paths.resource(modelName, modelPlural, resource)
+    }
+  }
+
+}
