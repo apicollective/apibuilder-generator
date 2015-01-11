2d984ec2c3c151561b812edb960930ac9c3ea12f
Michael Bryzek <michael@gilt.com>
2015-01-11 10:34:44 -0500

    Update to latest api

--- api/test/models/generator/ReferenceSpec.scala
+++ api/test/models/generator/ReferenceSpec.scala
@@ -5,17 +5,16 @@ import org.scalatest.{FunSpec, Matchers}
 
 class ReferenceSpec extends FunSpec with Matchers {
 
-  lazy val service = TestHelper.parseFile(s"reference-api/api.json")
-  lazy val ssd = new ScalaService(service)
+  lazy val ssd = new ScalaService(TestHelper.referenceApiService)
 
   it("user case classes") {
-    val model = ssd.models.find(_.name == "user").get
+    val model = ssd.models.find(_.name == "User").get
     val code = ScalaCaseClasses.generateCaseClass(model)
     TestHelper.assertEqualsFile("test/resources/generators/reference-spec-user-case-class.txt", code)
   }
 
   it("member case classes") {
-    val model = ssd.models.find(_.name == "member").get
+    val model = ssd.models.find(_.name == "Member").get
     val code = ScalaCaseClasses.generateCaseClass(model)
     TestHelper.assertEqualsFile("test/resources/generators/reference-spec-member-case-class.txt", code)
   }
