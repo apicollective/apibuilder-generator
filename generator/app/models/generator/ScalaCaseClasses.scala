23b9d0cd65476ed42a3d7bf4d796bc2abfb15e99
Michael Bryzek <michael@gilt.com>
2015-02-02 17:00:39 -0500

    Revert "First pass at scala traits for union types"
    This reverts commit 9e395d93e555978f224097837d4cde9ffddb132e.

--- generator/app/models/generator/ScalaCaseClasses.scala
+++ generator/app/models/generator/ScalaCaseClasses.scala
@@ -41,8 +41,9 @@ object ScalaCaseClasses extends CodeGenerator {
     val unionModels = union.types.flatMap { t => models.find(_.qualifiedName == t.name) }
 
     val commonFields = if (!unionModels.isEmpty && unionModels.map(_.name).sorted == union.types.map(_.primitive.shortName).sorted) {
-      val allFields = unionModels.flatMap { _.fields.map( f => s"${f.definition(f.name)}" ) }.groupBy(_)
-      val selected = allFields.filter(_._2.size == unionModels.size).map(_._1).toSeq
+      val allFields = unionModels.flatMap { _.fields.map { f => FieldType(f.name, f.datatype) } }.groupBy(ft => ft.name + ":" + ft.datatype.name)
+
+      val selected = allFields.filter(_._2.size == unionModels.size).map(_._2.head.name).toSeq
 
       unionModels.head.fields.filter(field => selected.contains(field.name)) match {
         case Nil => ""
