7101ac0e0cd4cdabb0c07eb321bc5e2df8bb0ae1
Michael Bryzek <michael@gilt.com>
2015-02-23 17:25:59 -0500

    Add snapshot of generated code for reference spec for regression testing

--- generator/test/models/generator/ReferenceSpec.scala
+++ generator/test/models/generator/ReferenceSpec.scala
@@ -1,5 +1,9 @@
 package generator
 
+import com.gilt.apidoc.generator.v0.models.InvocationForm
+import models.{Play23ClientGenerator, RubyClientGenerator}
+import models.ning.Ning18ClientGenerator
+
 import models.TestHelper
 import org.scalatest.{FunSpec, Matchers}
 
@@ -17,6 +21,21 @@ class ReferenceSpec extends FunSpec with Matchers {
     val model = ssd.models.find(_.name == "Member").get
     val code = ScalaCaseClasses.generateCaseClass(model, Seq.empty)
     TestHelper.assertEqualsFile("test/resources/generators/reference-spec-member-case-class.txt", code)
+  }
+
+  it("generates expected code for play 2.3 client") {
+    val code = Play23ClientGenerator.invoke(InvocationForm(service = TestHelper.referenceApiService))
+    TestHelper.assertEqualsFile("test/resources/generators/reference-spec-play-23.txt", code)
+  }
+
+  it("generates expected code for ning client") {
+    val code = Ning18ClientGenerator.invoke(InvocationForm(service = TestHelper.referenceApiService))
+    TestHelper.assertEqualsFile("test/resources/generators/reference-spec-ning-client.txt", code)
+  }
+
+  it("generates expected code for ruby client") {
+    val code = RubyClientGenerator.invoke(InvocationForm(service = TestHelper.referenceApiService))
+    TestHelper.assertEqualsFile("test/resources/generators/reference-spec-ruby-client.txt", code)
   }
 
 }
