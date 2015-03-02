4f4326caedf56cb5a6db2bf74b4ab90eb8074798
Michael Bryzek <michael@gilt.com>
2015-03-02 10:31:46 -0500

    Use ruby constants. Update test output

--- generator/app/models/Headers.scala
+++ generator/app/models/Headers.scala
@@ -49,23 +49,20 @@ case class Headers(
     ).mkString("\n\n")
   }
 
-  val scala = joinHeaders(".").map { case (name, value) =>
-    if (name == VersionMajorHeaderName) {
-      (name, s"${value}.toString")
-    } else {
-      (name, value)
-    }
+  val scala = Seq(
+    Some("User-Agent" -> s"Constants.UserAgent"),
+    Some("X-Apidoc-Version" -> s"Constants.Version"),
+    versionMajor.map { major => VersionMajorHeaderName -> s"Constants.VersionMajor.toString" }
+  ).flatten ++ form.service.headers.filter(!_.default.isEmpty).map { h =>
+    (h.name -> ScalaUtil.wrapInQuotes(h.default.get))
   }
-  val ruby = joinHeaders("::")
 
-  private def joinHeaders(separator: String) = {
-    Seq(
-      Some("User-Agent" -> s"Constants${separator}UserAgent"),
-      Some("X-Apidoc-Version" -> s"Constants${separator}Version"),
-      versionMajor.map { major => VersionMajorHeaderName -> s"Constants${separator}VersionMajor" }
-    ).flatten ++ form.service.headers.filter(!_.default.isEmpty).map { h =>
-      (h.name -> ScalaUtil.wrapInQuotes(h.default.get))
-    }
+  val ruby = Seq(
+    Some("User-Agent" -> s"Constants::USER_AGENT"),
+    Some("X-Apidoc-Version" -> s"Constants::VERSION"),
+    versionMajor.map { major => VersionMajorHeaderName -> s"Constants::VERSION_MAJOR" }
+  ).flatten ++ form.service.headers.filter(!_.default.isEmpty).map { h =>
+    (h.name -> RubyUtil.wrapInQuotes(h.default.get))
   }
 
 }
