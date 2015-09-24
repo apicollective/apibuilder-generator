fb9a2499785d52a3ec7fd7c735d36aeef3d2b78b
Michael Bryzek <mbryzek@alum.mit.edu>
2015-09-24 13:50:10 -0400

    All imports are from models package

--- scala-generator/src/main/scala/models/generator/Namespaces.scala
+++ scala-generator/src/main/scala/models/generator/Namespaces.scala
@@ -24,25 +24,7 @@ case class Namespaces(original: String) {
   val last: String = base.split("\\.").last
 
   def importStatements(service: Service): Seq[String] = {
-    Seq(
-      if (service.enums.isEmpty) {
-        None
-      } else {
-        Some(enums)
-      },
-
-      if (service.models.isEmpty) {
-        None
-      } else {
-        Some(models)
-      },
-
-      if (service.unions.isEmpty) {
-        None
-      } else {
-        Some(unions)
-      }
-).flatten.map { ns => s"import ${ns}._" }
+    Seq(s"import ${models}._")
   }
 
 }
