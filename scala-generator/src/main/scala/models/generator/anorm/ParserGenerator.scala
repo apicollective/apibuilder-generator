692f50c8bb6ad062f3198fc4b8c037d3ee1fe575
Michael Bryzek <mbryzek@alum.mit.edu>
2015-11-12 17:59:22 -0500

    Check for services with no models

--- scala-generator/src/main/scala/models/generator/anorm/ParserGenerator.scala
+++ scala-generator/src/main/scala/models/generator/anorm/ParserGenerator.scala
@@ -1,6 +1,6 @@
 package scala.generator.anorm
 
-import com.bryzek.apidoc.spec.v0.models.Service
+import com.bryzek.apidoc.spec.v0.models.{Model, Service}
 import com.bryzek.apidoc.generator.v0.models.{File, InvocationForm}
 import lib.generator.CodeGenerator
 // import lib.Text._
@@ -26,7 +26,24 @@ object ParserGenerator extends CodeGenerator {
   }
 
   private[this] def generateCode(service: Service): Option[String] = {
-    None
+    service.models.map(generateModelParser(_)) match {
+      case Nil => {
+        None
+      }
+      case models => {
+        Some(
+          Seq(
+            "package TODO {",
+            models.mkString("\n"),
+            "}"
+          ).mkString("\n")
+        )
+      }
+    }
+  }
+
+  private[this] def generateModelParser(model: Model): String = {
+    "TODO"
   }
 
 }
