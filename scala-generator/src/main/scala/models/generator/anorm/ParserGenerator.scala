f675f235896b03f9837f784c95f296993e5cd6ab
Michael Bryzek <mbryzek@alum.mit.edu>
2015-11-13 08:36:04 -0500

    First pass at generated row parser for primitive types

--- scala-generator/src/main/scala/models/generator/anorm/ParserGenerator.scala
+++ scala-generator/src/main/scala/models/generator/anorm/ParserGenerator.scala
@@ -1,6 +1,6 @@
 package scala.generator.anorm
 
-import scala.generator.{ScalaModel, ScalaService}
+import scala.generator.{ScalaField, ScalaModel, ScalaPrimitive, ScalaService}
 import com.bryzek.apidoc.spec.v0.models.Service
 import com.bryzek.apidoc.generator.v0.models.{File, InvocationForm}
 import lib.generator.CodeGenerator
@@ -64,13 +64,59 @@ object ParserGenerator extends CodeGenerator {
   }
 
   private[this] def generateModelParser(model: ScalaModel): String = {
+    println(s"generateMoodelParser(${model.name})")
     Seq(
       "def parser(",
       model.fields.map { f => s"${f.name}: String" }.mkString(",\n").indent(2),
       s"): RowParser[${model.qualifiedName}] = {",
-      "//TODO".indent(2),
+      Seq(
+        model.fields.map { generateRowParser(_) }.mkString(" ~\n") + " map {",
+        Seq(
+          "case " + model.fields.map(_.name).mkString(" ") + " => {",
+          Seq(
+            s"${model.qualifiedName}(",
+            model.fields.map { f =>
+              generateInstance(f)
+            }.mkString("\n").indent(2),
+            ")"
+          ).mkString("\n").indent(2),
+          "}"
+        ).mkString("\n").indent(2),
+        "}"
+      ).mkString("\n").indent(2),
       "}"
     ).mkString("\n")
+  }
+
+  private[this] def generateRowParser(field: ScalaField) = {
+    field.datatype match {
+      case f @ ScalaPrimitive.Boolean => generatePrimitiveRowParser(field.name, f)
+      case f @ ScalaPrimitive.Double => generatePrimitiveRowParser(field.name, f)
+      case f @ ScalaPrimitive.Integer => generatePrimitiveRowParser(field.name, f)
+      case f @ ScalaPrimitive.Long => generatePrimitiveRowParser(field.name, f)
+      case f @ ScalaPrimitive.DateIso8601 => generatePrimitiveRowParser(field.name, f)
+      case f @ ScalaPrimitive.DateTimeIso8601 => generatePrimitiveRowParser(field.name, f)
+      case f @ ScalaPrimitive.Decimal => generatePrimitiveRowParser(field.name, f)
+      case f @ ScalaPrimitive.Object => generatePrimitiveRowParser(field.name, f)
+      case f @ ScalaPrimitive.String => generatePrimitiveRowParser(field.name, f)
+      case f @ ScalaPrimitive.Unit => generatePrimitiveRowParser(field.name, f)
+      case f @ ScalaPrimitive.Uuid => generatePrimitiveRowParser(field.name, f)
+      // TODO: case ScalaPrimitive.Model(_, _) | ScalaPrimitive.Enum(_, _) | ScalaPrimitive.Union(_, _) => {
+    }
+  }
+
+  private[this] def generatePrimitiveRowParser(field: String, datatype: ScalaPrimitive) = {
+    s"SqlParser.get[${datatype.fullName}]($field)"
+  }
+
+  private[this] def generateInstance(field: ScalaField) = {
+    println(s"generateInstance(field): name[${field.name}] type[${field.datatype}]")
+    field.datatype match {
+      case ScalaPrimitive.Boolean | ScalaPrimitive.Double | ScalaPrimitive.Integer | ScalaPrimitive.Long | ScalaPrimitive.DateIso8601 | ScalaPrimitive.DateTimeIso8601 | ScalaPrimitive.Decimal | ScalaPrimitive.Object | ScalaPrimitive.String | ScalaPrimitive.Unit | ScalaPrimitive.Uuid => {
+        s"${field.name} = ${field.name}"
+      }
+        // TODO: case ScalaPrimitive.Model(_, _) | ScalaPrimitive.Enum(_, _) | ScalaPrimitive.Union(_, _) => {
+    }
   }
 
 }
