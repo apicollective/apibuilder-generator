ea7b78ff153443dea903e4578fec707f557cc481
Michael Bryzek <mbryzek@alum.mit.edu>
2016-01-09 14:55:33 -0500

    Remove debugging output

--- example-union-types-discriminator/api/app/controllers/Users.scala
+++ example-union-types-discriminator/api/app/controllers/Users.scala
@@ -54,9 +54,6 @@ object Users extends Controller {
       case s: JsSuccess[User] => {
         val user = s.get
 
-        println(s"json: "+ request.body.toString)
-        println(s"user: " +user)
-
         user match {
           case RegisteredUser(id, email) => println(s"Received Registered User $id")
           case GuestUser(id, email) => println(s"Received Guest User $id")
