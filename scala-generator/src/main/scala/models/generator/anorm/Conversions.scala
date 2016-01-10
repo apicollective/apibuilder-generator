5dd89d27160a13bb0343664ae0c80808322856ab
Michael Bryzek <mbryzek@alum.mit.edu>
2016-01-09 23:56:16 -0500

    Always include Local package for consistent importing

--- scala-generator/src/main/scala/models/generator/anorm/Conversions.scala
+++ scala-generator/src/main/scala/models/generator/anorm/Conversions.scala
@@ -72,34 +72,32 @@ package %s {
   ): String = {
 
     Seq(
-      Some(Header.format(namespaces.anormConversions)),
-      Some(
-        Types.map { t =>
-          Seq(
-            s"implicit val columnToSeq${t.shortName}: Column[Seq[${t.fullName}]] = parser { _.as[Seq[${t.fullName}]] }",
-            s"implicit val columnToMap${t.shortName}: Column[Map[String, ${t.fullName}]] = parser { _.as[Map[String, ${t.fullName}]] }"
-          ).mkString("\n").indent(2)
-        }.mkString("\n").indent(2)
-      ),
-      unions match {
-        case Nil => None
-        case _ => {
-          Some(
-            Seq(
-              s"object Local {",
+      Header.format(namespaces.anormConversions),
+      Types.map { t =>
+        Seq(
+          s"implicit val columnToSeq${t.shortName}: Column[Seq[${t.fullName}]] = parser { _.as[Seq[${t.fullName}]] }",
+          s"implicit val columnToMap${t.shortName}: Column[Map[String, ${t.fullName}]] = parser { _.as[Map[String, ${t.fullName}]] }"
+        ).mkString("\n").indent(2)
+      }.mkString("\n").indent(2),
+      Seq(
+        Some(s"object Local {"),
+        unions match {
+          case Nil => None
+          case _ => {
+            Some(
               Seq(
                 s"import ${namespaces.models}.json._",
                 unions.map { u =>
                   s"implicit val columnTo${u.name}: Column[${u.qualifiedName}] = parser { _.as[${u.qualifiedName}] }"
                 }.mkString("\n")
-              ).mkString("\n").indent(2),
-              "}"
-            ).mkString("\n\n").indent(4)
-          )
-        }
-      },
-      Some(Footer)
-    ).flatten.mkString("\n\n")
+              ).mkString("\n").indent(2)
+            )
+          }
+        },
+        Some("}")
+      ).flatten.mkString("\n"),
+      Footer
+    ).mkString("\n\n")
 
   }
 
