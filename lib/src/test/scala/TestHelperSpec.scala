97a928df5eaeddc134b3142580e72f70e95d73a8
Sean C. Sullivan <ssullivan@gilt.com>
2017-06-25 07:13:38 -0700

    add unit tests for TestHelper.assertValidScalaSourceCode

--- lib/src/test/scala/TestHelperSpec.scala
+++ lib/src/test/scala/TestHelperSpec.scala
@@ -2,6 +2,8 @@ package lib
 
 import scala.util.{Failure, Success, Try}
 import org.scalatest.{FunSpec, Matchers}
+import models.TestHelper
+import org.scalatest.exceptions.TestFailedException
 
 class TestHelperSpec extends FunSpec with Matchers {
 
@@ -25,4 +27,22 @@ class TestHelperSpec extends FunSpec with Matchers {
     errors should be(Nil)
   }
 
+  describe("assertValidScalaSourceCode") {
+    it("accepts valid Scala source code") {
+      val code = """
+          |import java.util.Calendar
+          |import scala.concurrent.Future
+          |
+          |trait MyTrait
+          |
+          |class Foobar extends MyTrait
+          |
+        """.stripMargin
+      TestHelper.assertValidScalaSourceCode(code)
+    }
+
+    it("throws exception when source code is invalid") {
+      intercept[TestFailedException] { TestHelper.assertValidScalaSourceCode("invalid Scala code") }
+    }
+  }
 }
