d354d248151afb126afcea7c1c4bccbe55d32a6b
Sean Sullivan <ssullivan@gilt.com>
2017-08-17 20:06:09 -0400

    add KotlinTestHelper.scala

---
+++ kotlin-generator/src/test/scala/models/generator/kotlin/KotlinTestHelper.scala
@@ -0,0 +1,33 @@
+package models.generator.kotlin
+
+import java.util.concurrent.atomic.AtomicInteger
+
+import org.jetbrains.kotlin.cli.common.messages.{CompilerMessageLocation, CompilerMessageSeverity, MessageCollector}
+import org.jetbrains.kotlin.cli.common.arguments.K2JVMCompilerArguments
+import org.jetbrains.kotlin.cli.jvm.K2JVMCompiler
+import org.jetbrains.kotlin.config.Services
+import org.scalatest.Matchers
+
+object KotlinTestHelper extends Matchers {
+
+  def assertValidKotlinSourceCode(sourceCode: String): Unit = {
+    val msgCollector = new MessageCollectorImpl()
+    val compiler = new K2JVMCompiler()
+    val args = new K2JVMCompilerArguments()
+    val exitCode = compiler.exec(msgCollector, Services.EMPTY, args)
+    msgCollector.hasErrors shouldBe false
+  }
+
+  class MessageCollectorImpl extends MessageCollector {
+    private val errorCount = new AtomicInteger(0)
+    override def hasErrors: Boolean = { errorCount.get > 0 }
+    override def clear(): Unit = ???
+    override def report(severity: CompilerMessageSeverity, message: String, location: CompilerMessageLocation): Unit = {
+      if (severity.isError) {
+        errorCount.incrementAndGet()
+        System.err.println("KotlinTestHelper ERROR: " + message)
+      }
+    }
+
+  }
+}
