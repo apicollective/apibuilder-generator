30195afc87af6154734fe4a342346de36af282dc
Gregor Heine <gheine@gilt.com>
2017-09-01 11:50:39 +0100

    Http4s server: Various query param extractor fixes

--- scala-generator/src/main/scala/models/http4s/server/Http4sServer.scala
+++ scala-generator/src/main/scala/models/http4s/server/Http4sServer.scala
@@ -5,6 +5,7 @@ import io.apibuilder.generator.v0.models.InvocationForm
 import scala.generator.{ScalaClientMethodConfigs, ScalaDatatype, ScalaParameter, ScalaPrimitive, ScalaUtil}
 import scala.models.JsonImports
 import scala.models.http4s.{ScalaGeneratorUtil, ScalaService}
+import lib.Text
 import lib.Text._
 import lib.VersionTag
 
@@ -35,21 +36,23 @@ object Http4sServer {
   }
 
   def queryExtractor(param: ScalaParameter): QueryExtractor = {
-    def recurse(typ: ScalaDatatype, collPart: String): QueryExtractor = {
+    def recurse(typ: ScalaDatatype, collPart: String, withMinMaxDef: Boolean): QueryExtractor = {
       typ match {
-        case sp: ScalaPrimitive =>
+        case sp: ScalaPrimitive if withMinMaxDef =>
           val minPart = param.param.minimum.fold("")(v => s"$v")
           val maxPart = param.param.maximum.fold("")(v => s"To$v")
-          val defPart = param.default.fold("")(v => s"Def$v")
+          val defPart = param.param.default.fold("")(v => s"Def${ScalaUtil.toClassName(v)}")
           QueryExtractor(s"${param.name.capitalize}$collPart${sp.shortName}$minPart$maxPart${defPart}Matcher", param.name)
+        case sp: ScalaPrimitive =>
+          QueryExtractor(s"${param.name.capitalize}$collPart${sp.shortName}Matcher", param.name)
         case ScalaDatatype.List(nested) =>
-          val extractor = recurse(nested, "List")
+          val extractor = recurse(nested, "List", false)
           QueryExtractor(extractor.name, s"cats.data.Validated.Valid(${extractor.handler})")
         case ScalaDatatype.Option(nested) =>
-          recurse(nested, "Opt")
+          recurse(nested, "Opt", true)
       }
     }
-    recurse(param.datatype, "")
+    recurse(param.datatype, "", true)
   }
 
 }
@@ -95,6 +98,8 @@ case class Http4sServer(form: InvocationForm,
       val name = lib.Text.snakeToCamelCase(lib.Text.camelCaseToUnderscore(ScalaUtil.toClassName(resource.resource.`type`)).toLowerCase + "_routes").capitalize
 
       s"""trait $name {
+         |  import Matchers._
+         |
          |  implicit def circeJsonDecoder[A](implicit decoder: io.circe.Decoder[A]) = org.http4s.circe.jsonOf[A]
          |  implicit def circeJsonEncoder[A](implicit encoder: io.circe.Encoder[A]) = org.http4s.circe.jsonEncoderOf[A]
          |
@@ -109,13 +114,41 @@ case class Http4sServer(form: InvocationForm,
          |""".stripMargin
     }.mkString("\n")
 
+    val enumDecoders = allRoutes.flatMap(_.op.queryParameters).map(_.datatype).collect {
+      case enum: ScalaPrimitive.Enum => enum
+      case ScalaDatatype.Option(enum: ScalaPrimitive.Enum) => enum
+      case ScalaDatatype.List(enum: ScalaPrimitive.Enum) => enum
+      case ScalaDatatype.Option(ScalaDatatype.List(enum: ScalaPrimitive.Enum)) => enum
+    }.distinct.map { enum =>
+      s"""
+         |implicit lazy val ${Text.initLowerCase(ssd.name)}${ScalaUtil.toClassName(enum.shortName)}QueryParamDecoder: org.http4s.QueryParamDecoder[${enum.fullName}] =
+         |  org.http4s.QueryParamDecoder.fromUnsafeCast[${enum.fullName}](p => ${enum.fullName}.fromString(p.value).get)("${enum.fullName}")""".stripMargin
+    }.mkString("\n")
+
     s"""package ${ssd.namespaces.base}.server
        |
        |import org.http4s.dsl._
        |${JsonImports(form.service).mkString("\n")}
-       |$versionCapture
-       |${path.mkString("\n")}
-       |${query.mkString("\n")}
+       |
+       |private[server] object Matchers {
+       |
+       |  implicit lazy val bigDecimalDateQueryParamDecoder: org.http4s.QueryParamDecoder[BigDecimal] =
+       |    org.http4s.QueryParamDecoder.fromUnsafeCast[BigDecimal](p => BigDecimal(p.value))("BigDecimal")
+       |
+       |  implicit lazy val instantQueryParamDecoder: org.http4s.QueryParamDecoder[java.time.Instant] =
+       |    org.http4s.QueryParamDecoder.fromUnsafeCast[java.time.Instant](p => java.time.Instant.parse(p.value))("java.time.Instant")
+       |
+       |  implicit lazy val localDateQueryParamDecoder: org.http4s.QueryParamDecoder[java.time.LocalDate] =
+       |    org.http4s.QueryParamDecoder.fromUnsafeCast[java.time.LocalDate](p => java.time.LocalDate.parse(p.value))("java.time.LocalDate")
+       |
+       |  implicit lazy val uuidQueryParamDecoder: org.http4s.QueryParamDecoder[java.util.UUID] =
+       |    org.http4s.QueryParamDecoder.fromUnsafeCast[java.util.UUID](p => java.util.UUID.fromString(p.value))("java.util.UUID")
+       |${enumDecoders.indent(2)}
+       |${versionCapture.indent(2)}
+       |${path.mkString("\n").indent(2)}
+       |${query.mkString("\n").indent(2)}
+       |}
+       |
        |$resources
      """.stripMargin
   }
@@ -125,10 +158,10 @@ case class Http4sServer(form: InvocationForm,
       case Extracted(_, param) => Http4sServer.pathExtractor(param) -> param
     }.toMap.toList.sortBy(_._1.name)
 
-    distinctParams.map { case (extractor, parameter) =>
+    distinctParams.map { case (extractor, param) =>
       val filter = extractor.filter.getOrElse("")
 
-      val toOption = parameter.datatype match {
+      val toOption = param.datatype match {
         case ScalaPrimitive.String => s"Some(s)$filter"
         case ScalaPrimitive.Boolean => "scala.util.Try(s.toBoolean).toOption"
         case ScalaPrimitive.Integer => s"scala.util.Try(s.toInt).toOption$filter"
@@ -139,34 +172,44 @@ case class Http4sServer(form: InvocationForm,
         case ScalaPrimitive.DateIso8601Java => "scala.util.Try(java.time.LocalDate.parse(s)).toOption"
         case ScalaPrimitive.DateTimeIso8601Java => "scala.util.Try(java.time.Instant.parse(s)).toOption"
         case enum: ScalaPrimitive.Enum => s"${enum.name}.fromString(s)"
-        case _ => s"""None // Type ${parameter.datatype.name} is not supported as a capture value"""
+        case _ => s"""None // Type ${param.datatype.name} is not supported as a capture value"""
       }
 
       Seq(
         s"object ${extractor.name} {",
-        s"  def unapply(s: String): Option[${parameter.datatype.name}] = $toOption",
+        s"  def unapply(s: String): Option[${param.datatype.name}] = $toOption",
         "}\n"
       ).mkString("\n")
     }
   }
 
   def genQueryExtractors(routes: List[Route]): Seq[String] = {
-    val distinctParams = routes.flatMap(_.op.queryParameters).groupBy { param =>
-      val typ = param.datatype match {
-        case ScalaDatatype.Option(container: ScalaDatatype.Container) => container
-        case other => other
-      }
-      (param.originalName, typ, param.param.minimum, param.param.maximum, param.default)
-    }.values.toList.map(_.head)
-    distinctParams.map { param =>
-      val extractorName = Http4sServer.queryExtractor(param).name
+    val distinctParams = routes.flatMap(_.op.queryParameters).map { param =>
+      Http4sServer.queryExtractor(param) -> param
+    }.toMap.toList.sortBy(_._1.name)
+    distinctParams.map { case (QueryExtractor(extractorName, _), param) =>
       param.datatype match {
         case ScalaDatatype.Option(ScalaDatatype.List(sp: ScalaPrimitive)) =>
-          s"""object $extractorName extends OptionalMultiQueryParamDecoderMatcher[${sp.shortName}]("${param.originalName}")"""
+          s"""object $extractorName extends OptionalMultiQueryParamDecoderMatcher[${sp.name}]("${param.originalName}")"""
         case ScalaDatatype.List(sp: ScalaPrimitive) =>
-          s"""object $extractorName extends OptionalMultiQueryParamDecoderMatcher[${sp.shortName}]("${param.originalName}")"""
+          s"""object $extractorName extends OptionalMultiQueryParamDecoderMatcher[${sp.name}]("${param.originalName}")"""
         case ScalaDatatype.Option(sp: ScalaPrimitive) =>
-          s"""object $extractorName extends OptionalQueryParamDecoderMatcher[${sp.shortName}]("${param.originalName}")"""
+          val filterPart = (sp, param.param.minimum, param.param.maximum) match {
+            case (ScalaPrimitive.Integer | ScalaPrimitive.Long, Some(min), None) => Some(s".filter(_.forall(_ >= $min))")
+            case (ScalaPrimitive.Integer | ScalaPrimitive.Long, None, Some(max)) => Some(s".filter(_.forall(_ <= $max))")
+            case (ScalaPrimitive.Integer | ScalaPrimitive.Long, Some(min), Some(max)) => Some(s".filter(_.forall(v => v >= $min && v <= $max))")
+            case (ScalaPrimitive.String, Some(min), None) => Some(s".filter(_.forall(_.length >= $min))")
+            case (ScalaPrimitive.String, None, Some(max)) => Some(s".filter(_.forall(_.length <= $max))")
+            case (ScalaPrimitive.String, Some(min), Some(max)) => Some(s".filter(_.forall(s => s.length >= $min && s.length <= $max))")
+            case _ => None
+          }
+          filterPart.fold(
+            s"""object $extractorName extends OptionalQueryParamDecoderMatcher[${sp.name}]("${param.originalName}")"""
+          ){ filterPart =>
+            s"""object $extractorName extends OptionalQueryParamDecoderMatcher[${sp.name}]("${param.originalName}") {
+               |  override def unapply(params: Map[String, Seq[String]]) = super.unapply(params)$filterPart
+               |}""".stripMargin
+          }
         case sp: ScalaPrimitive =>
           val defPart = param.default.map { d =>
             val default = sp match {
@@ -188,13 +231,12 @@ case class Http4sServer(form: InvocationForm,
 
 
           if (defPart.isDefined || filterPart.isDefined) {
-            Seq(
-              s"""object $extractorName extends QueryParamDecoderMatcher[${sp.shortName}]("${param.originalName}") {""",
-              s"""  override def unapply(params: Map[String, Seq[String]]) = super.unapply(params)${defPart.getOrElse("")}${filterPart.getOrElse("")}""",
-              s"""}"""
-            ).mkString("\n")
+
+            s"""object $extractorName extends QueryParamDecoderMatcher[${sp.name}]("${param.originalName}") {
+               |  override def unapply(params: Map[String, Seq[String]]) = super.unapply(params)${defPart.getOrElse("")}${filterPart.getOrElse("")}
+               |}""".stripMargin
           } else {
-            s"""object $extractorName extends QueryParamDecoderMatcher[${sp.shortName}]("${param.originalName}")"""
+            s"""object $extractorName extends QueryParamDecoderMatcher[${sp.name}]("${param.originalName}")"""
           }
       }
     }.map(_ + "\n")
