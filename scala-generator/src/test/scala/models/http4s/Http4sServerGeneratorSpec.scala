6c195ef02f29ddaf459bf3c179eb9309c8b4b916
Gregor Heine <gheine@gilt.com>
2017-09-02 14:07:10 +0100

    Http4s server: Add test for response types (#258)

--- scala-generator/src/test/scala/models/http4s/Http4sServerGeneratorSpec.scala
+++ scala-generator/src/test/scala/models/http4s/Http4sServerGeneratorSpec.scala
@@ -38,4 +38,14 @@ class Http4sServerGeneratorSpec extends FunSpec with ShouldMatchers {
     assertValidScalaSourceCode(scalaSourceCode)
     models.TestHelper.assertEqualsFile("/http4s/query-params-017.txt", scalaSourceCode)
   }
+
+  it("response types test generates expected code for http4s 0.17 server") {
+    val service = models.TestHelper.parseFile(s"/http4s/response-types.json")
+    val server = Http4sServer(new InvocationForm(service, Seq.empty, None),
+      new ScalaService(service),
+      new ScalaClientMethodConfigs.Http4s017(namespace = "whatever", baseUrl = None))
+    val scalaSourceCode = server.generate()
+    assertValidScalaSourceCode(scalaSourceCode)
+    models.TestHelper.assertEqualsFile("/http4s/response-types-017.txt", scalaSourceCode)
+  }
 }
