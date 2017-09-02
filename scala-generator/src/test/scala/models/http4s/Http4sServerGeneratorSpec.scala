a3dd8b10d5ef428e06667a944316f5ad5499ca20
Gregor Heine <gheine@gilt.com>
2017-09-02 09:19:08 +0100

    Http4s server: Various query param extractor fixes (#256)

--- scala-generator/src/test/scala/models/http4s/Http4sServerGeneratorSpec.scala
+++ scala-generator/src/test/scala/models/http4s/Http4sServerGeneratorSpec.scala
@@ -1,6 +1,7 @@
 package scala.models.http4s
 
 import io.apibuilder.generator.v0.models.InvocationForm
+import models.TestHelper.assertValidScalaSourceCode
 import org.scalatest.{FunSpec, ShouldMatchers}
 
 import scala.generator.ScalaClientMethodConfigs
@@ -8,20 +9,33 @@ import scala.models.http4s.server.Http4sServer
 
 class Http4sServerGeneratorSpec extends FunSpec with ShouldMatchers {
 
-  it("generates expected code for http4s 0.15 server path parameters") {
+  it("path parameters test generates expected code for http4s 0.15 server") {
     val service = models.TestHelper.parseFile(s"/http4s/path-params.json")
     val server = Http4sServer(new InvocationForm(service, Seq.empty, None),
                               new ScalaService(service),
                               new ScalaClientMethodConfigs.Http4s015(namespace = "whatever", baseUrl = None))
-    models.TestHelper.assertEqualsFile("/http4s/path-params-015.txt", server.generate())
+    val scalaSourceCode = server.generate()
+    assertValidScalaSourceCode(scalaSourceCode)
+    models.TestHelper.assertEqualsFile("/http4s/path-params-015.txt", scalaSourceCode)
   }
 
-  it("generates expected code for http4s 0.17 server path parameters") {
+  it("path parameters test generates expected code for http4s 0.17 server") {
     val service = models.TestHelper.parseFile(s"/http4s/path-params.json")
     val server = Http4sServer(new InvocationForm(service, Seq.empty, None),
       new ScalaService(service),
       new ScalaClientMethodConfigs.Http4s017(namespace = "whatever", baseUrl = None))
-    models.TestHelper.assertEqualsFile("/http4s/path-params-017.txt", server.generate())
+    val scalaSourceCode = server.generate()
+    assertValidScalaSourceCode(scalaSourceCode)
+    models.TestHelper.assertEqualsFile("/http4s/path-params-017.txt", scalaSourceCode)
   }
 
+  it("query parameters test generates expected code for http4s 0.17 server") {
+    val service = models.TestHelper.parseFile(s"/http4s/query-params.json")
+    val server = Http4sServer(new InvocationForm(service, Seq.empty, None),
+      new ScalaService(service),
+      new ScalaClientMethodConfigs.Http4s017(namespace = "whatever", baseUrl = None))
+    val scalaSourceCode = server.generate()
+    assertValidScalaSourceCode(scalaSourceCode)
+    models.TestHelper.assertEqualsFile("/http4s/query-params-017.txt", scalaSourceCode)
+  }
 }
