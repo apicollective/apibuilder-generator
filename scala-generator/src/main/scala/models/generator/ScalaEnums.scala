8abc7863920d425ac4a154c6596272a2ac002a67
Alex <alex@flow.io>
2018-01-04 14:29:12 -0500

    + Addresses the case when persisting enum values as strings and then newing up enum based on those values when enum was removed from API but persistence store was not     updated to be in sync, which quietly results in UNDEFINED(removed_enum_string_here) that could potentially cause application errors or undesired behavior.

--- scala-generator/src/main/scala/models/generator/ScalaEnums.scala
+++ scala-generator/src/main/scala/models/generator/ScalaEnums.scala
@@ -46,7 +46,10 @@ case class UNDEFINED(override val toString: String) extends ${enum.name}
     s"val all: scala.List[${enum.name}] = scala.List(" + enum.values.map(_.name).mkString(", ") + ")\n\n" +
     s"private[this]\n" +
     s"val byName: Map[String, ${enum.name}] = all.map(x => x.toString.toLowerCase -> x).toMap\n\n" +
-    s"def apply(value: String): ${enum.name} = fromString(value).getOrElse(UNDEFINED(value))\n\n" +
+      s"def apply(value: String)(implicit onUndefined: String => Unit = _ => {}): ${enum.name} = fromString(value).getOrElse {\n" +
+      s"\tonUndefined(value)\n" +
+      s"\tUNDEFINED(value)\n" +
+      s"}\n\n" +
     s"def fromString(value: String): _root_.scala.Option[${enum.name}] = byName.get(value.toLowerCase)\n\n"
   }
 
