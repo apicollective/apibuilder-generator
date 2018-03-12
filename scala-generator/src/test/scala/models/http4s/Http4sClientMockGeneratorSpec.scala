9e1fa9438984291d83804b485377030c8e415366
Fabio Cognigni <fcognigni@gilt.com>
2018-03-12 11:55:11 +0000

    Client Mock generation for http4s 0.18. Fixes #362. (#375)
    * Client Mock generation for http4s 0.18 (cats 1.X). Fixes #362.
    
    * Removed println from test.
    
    * Renamed the client mock generator
    
    * Changed val into def
    
    * Reworked tests to use directly the generator (same as the app).

---
+++ scala-generator/src/test/scala/models/http4s/Http4sClientMockGeneratorSpec.scala
@@ -0,0 +1,40 @@
+package models.http4s
+
+import io.apibuilder.generator.v0.models.InvocationForm
+import models.TestHelper.assertValidScalaSourceCode
+import org.scalatest.{FunSpec, Matchers}
+
+import scala.generator.ScalaClientMethodConfigs
+import scala.models.http4s.{Http4s017Generator, Http4s018Generator, ScalaService}
+
+class Http4sClientMockGeneratorSpec extends FunSpec with Matchers {
+
+  describe("generate mock") {
+    val service = models.TestHelper.generatorApiService
+    val ssd = new ScalaService(models.TestHelper.generatorApiService)
+    val invocationForm = new InvocationForm(service, Seq.empty, None)
+
+    it("Http4s 0.18 mock generator produces valid Scala source code") {
+      val config = new ScalaClientMethodConfigs.Http4s018(namespace = "whatever", baseUrl = None)
+
+      val sourceCode = Http4s018Generator.generateMockClientCode(invocationForm, ssd, config)
+
+      assertValidScalaSourceCode(sourceCode)
+      sourceCode should include("Applicative[F]")
+      sourceCode should include("F[_]")
+      sourceCode should include("Applicative[F].pure")
+    }
+
+    it("Http4s 0.17 mock generator produces valid Scala source code") {
+      val config = new ScalaClientMethodConfigs.Http4s017(namespace = "whatever", baseUrl = None)
+
+      val sourceCode = Http4s017Generator.generateMockClientCode(invocationForm, ssd, config)
+
+      assertValidScalaSourceCode(sourceCode)
+      sourceCode should not include ("Applicative[F]")
+      sourceCode should include("Task.now")
+
+    }
+  }
+
+}
