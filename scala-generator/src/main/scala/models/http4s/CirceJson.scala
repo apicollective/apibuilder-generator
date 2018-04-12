apicollective/apibuilder-generator

commit a67d47480160f2e26b17149edd9be56dbb487d72

Introducing defaults for required=false fields (#378)
* Introducing defaults to generated Play Reads implicits

* New API Spec generator

* Regenerate

* Regen properly

* WIP (Golang support)

* Flipped required bit

* Adding Circe generators

* Reverting tests

* Allowing two models of Scala required=false fields with defaults

* Cleaning up imports

* Fixes based on review

* Renaming model hints

scala-generator/src/main/scala/models/http4s/CirceJson.scala
@@ -149,24 +149,37 @@ ${Seq(generateEnums(), generateModels(), generateUnions()).filter(!_.isEmpty).mk
   private[models] def decodersAndEncoders(model: ScalaModel): String = {
     decoders(model) ++ "\n\n" ++ encoders(model)
   }
+  
   private[models] def decoders(model: ScalaModel): String = {
+    // backticks don't work correctly as enumerator names in for comprehensions
+    def nobt(fieldName:String) = fieldName.replaceAll("`", "__")
     Seq(
       s"${implicitDecoderDef(model.name)} = Decoder.instance { c =>",
       s" for {",
       model.fields.map { field =>
         field.datatype match {
           case ScalaDatatype.Option(inner) => {
-            s"""${field.name} <- c.downField("${field.originalName}").as[Option[${inner.name}]]"""
+            s"""${nobt(field.name)} <- c.downField("${field.originalName}").as[Option[${inner.name}]]"""
+          }
+          case datatype if field.shouldApplyDefaultOnRead => {
+            s"""${nobt(field.name)} <- c.downField("${field.originalName}").as[Option[${datatype.name}]]"""
           }
           case datatype => {
-            s"""${field.name} <- c.downField("${field.originalName}").as[${datatype.name}]"""
+            s"""${nobt(field.name)} <- c.downField("${field.originalName}").as[${datatype.name}]"""
           }
         }
       }.mkString("\n").indent(4),
       s"  } yield {",
       s"    ${model.name}(",
-      model.fields.map { field =>
-        s"""${field.name} = ${field.name}"""
+
+      model
+        .fields
+        .map {field =>
+          if (field.shouldApplyDefaultOnRead){
+            s"""${nobt(field.name)} = ${field.name}.getOrElse(${field.default.get})"""
+          } else {
+            s"""${nobt(field.name)} = ${field.name}"""
+          }
       }.mkString(",\n").indent(6),
       s"    )",
       s"  }",
@@ -179,10 +192,10 @@ ${Seq(generateEnums(), generateModels(), generateUnions()).filter(!_.isEmpty).mk
       s"${implicitEncoderDef(model.name)} = Encoder.instance { t =>",
       s"  Json.fromFields(Seq(",
       model.fields.map { field =>
-        if (field.required) {
-          s"""Some("${field.originalName}" -> t.${field.name}.asJson)"""
-        } else {
+        if (field.shouldModelConcreteType) {
           s"""t.${field.name}.map(t => "${field.originalName}" -> t.asJson)"""
+        } else {
+          s"""Some("${field.originalName}" -> t.${field.name}.asJson)"""
         }
       }.mkString(",\n").indent(4),
       s"  ).flatten)",

generated/app/ApicollectiveApibuilderCommonV0Models.scala
@@ -1,7 +1,7 @@
 /**
  * Generated by API Builder - https://www.apibuilder.io
- * Service version: 0.14.9
- * apibuilder 0.14.3 app.apibuilder.io/apicollective/apibuilder-common/0.14.9/play_2_x_json
+ * Service version: 0.14.12
+ * apibuilder 0.14.3 app.apibuilder.io/apicollective/apibuilder-common/0.14.12/play_2_x_json
  */
 package io.apibuilder.common.v0.models {
 
@@ -229,7 +229,7 @@ package io.apibuilder.common.v0 {
       }
 
       override def unbind(key: String, value: T): String = {
-        converters.convert(value)
+        s"$key=${converters.convert(value)}"
       }
     }
 

generated/app/ApicollectiveApibuilderGeneratorV0Client.scala
@@ -1,7 +1,7 @@
 /**
  * Generated by API Builder - https://www.apibuilder.io
- * Service version: 0.14.9
- * apibuilder 0.14.3 app.apibuilder.io/apicollective/apibuilder-generator/0.14.9/play_2_6_client
+ * Service version: 0.14.12
+ * apibuilder 0.14.3 app.apibuilder.io/apicollective/apibuilder-generator/0.14.12/play_2_6_client
  */
 package io.apibuilder.generator.v0.models {
 
@@ -460,7 +460,7 @@ package io.apibuilder.generator.v0 {
       }
 
       override def unbind(key: String, value: T): String = {
-        converters.convert(value)
+        s"$key=${converters.convert(value)}"
       }
     }
 
@@ -496,8 +496,8 @@ package io.apibuilder.generator.v0 {
 
     val BaseUrl = "https://api.apibuilder.io"
     val Namespace = "io.apibuilder.generator.v0"
-    val UserAgent = "apibuilder 0.14.3 app.apibuilder.io/apicollective/apibuilder-generator/0.14.9/play_2_6_client"
-    val Version = "0.14.9"
+    val UserAgent = "apibuilder 0.14.3 app.apibuilder.io/apicollective/apibuilder-generator/0.14.12/play_2_6_client"
+    val Version = "0.14.12"
     val VersionMajor = 0
 
   }

generated/app/ApicollectiveApibuilderSpecV0Client.scala
@@ -1,7 +1,7 @@
 /**
  * Generated by API Builder - https://www.apibuilder.io
- * Service version: 0.14.9
- * apibuilder 0.14.3 app.apibuilder.io/apicollective/apibuilder-spec/0.14.9/play_2_6_client
+ * Service version: 0.14.12
+ * apibuilder 0.14.3 app.apibuilder.io/apicollective/apibuilder-spec/0.14.12/play_2_6_client
  */
 package io.apibuilder.spec.v0.models {
 
@@ -1442,7 +1442,7 @@ package io.apibuilder.spec.v0 {
       }
 
       override def unbind(key: String, value: T): String = {
-        converters.convert(value)
+        s"$key=${converters.convert(value)}"
       }
     }
 
@@ -1477,8 +1477,8 @@ package io.apibuilder.spec.v0 {
   object Constants {
 
     val Namespace = "io.apibuilder.spec.v0"
-    val UserAgent = "apibuilder 0.14.3 app.apibuilder.io/apicollective/apibuilder-spec/0.14.9/play_2_6_client"
-    val Version = "0.14.9"
+    val UserAgent = "apibuilder 0.14.3 app.apibuilder.io/apicollective/apibuilder-spec/0.14.12/play_2_6_client"
+    val Version = "0.14.12"
     val VersionMajor = 0
 
   }

generator/conf/routes
@@ -1,6 +1,6 @@
 # Generated by API Builder - https://www.apibuilder.io
-# Service version: 0.14.9
-# apibuilder 0.14.3 app.apibuilder.io/apicollective/apibuilder-generator/0.14.9/play_2_x_routes
+# Service version: 0.14.12
+# apibuilder 0.14.3 app.apibuilder.io/apicollective/apibuilder-generator/0.14.12/play_2_x_routes
 
 GET      /generators                 controllers.Generators.get(key: _root_.scala.Option[String], limit: Int ?= 100, offset: Int ?= 0)
 GET      /generators/:key            controllers.Generators.getByKey(key: String)

go-generator/src/main/scala/models/Code.scala
@@ -1,12 +1,11 @@
 package go.models
 
-import io.apibuilder.spec.v0.models.{Enum, Model, Parameter, ParameterLocation, Resource, Service, Union}
-import io.apibuilder.spec.v0.models.{ResponseCode, ResponseCodeInt, ResponseCodeOption, ResponseCodeUndefinedType}
-import io.apibuilder.generator.v0.models.{File, InvocationForm}
-import Formatter._
+import go.models.Formatter._
+import io.apibuilder.generator.v0.models.InvocationForm
+import io.apibuilder.spec.v0.models.{Enum, Model, Parameter, ParameterLocation, Resource, ResponseCodeInt, ResponseCodeOption, ResponseCodeUndefinedType, Union}
 import lib.Datatype
 import lib.generator.GeneratorUtil
-import lib.Text
+
 import scala.collection.mutable
 
 case class Code(form: InvocationForm) {
@@ -119,6 +118,16 @@ case class Code(form: InvocationForm) {
     val publicName = importBuilder.publicName(model.name)
     val privateName = GoUtil.privateName(model.name)
 
+    val defaults = model.fields.flatMap { f =>
+      (f.default, f.required) match {
+        case (Some(d), false) => {
+          val publicFieldName = importBuilder.publicName(f.name)
+          Defaults.mkGolangDefault(f.default, datatype(f.`type`, true), service, importBuilder).map(d=> s"$privateName.$publicFieldName = $d")
+        }
+        case _ => None
+      }
+    }
+
     Seq(
       GoUtil.textToComment(model.description) + s"type $publicName struct {",
       model.fields.map { f =>
@@ -127,7 +136,7 @@ case class Code(form: InvocationForm) {
         val json = Seq(
           (publicName == f.name) match {
             case true => None
-            case fasle => Some(f.name)
+            case false => Some(f.name)
           },
           f.required match {
             case true => None
@@ -140,7 +149,7 @@ case class Code(form: InvocationForm) {
 
         Seq(
           Some(publicName),
-          Some(GoType(importBuilder, datatype(f.`type`, f.required)).klass.localName),
+          Some(GoType(importBuilder, datatype(f.`type`, f.required || f.default.isDefined)).klass.localName),
           json
         ).flatten.mkString(" ")
       }.mkString("\n").table().indent(1),
@@ -149,9 +158,9 @@ case class Code(form: InvocationForm) {
       "",
       Seq(
         s"func ${publicName}FromMap(data interface{}) $publicName {",
-	Seq(
+	        Seq(
           "b, err := json.Marshal(data)",
-	  "if err == nil {",
+	        "if err == nil {",
           s"return ${publicName}FromJson(${bytes}.NewReader(b))".indent(1),
           "} else {",
           "panic(err)".indent(1),
@@ -159,15 +168,17 @@ case class Code(form: InvocationForm) {
         ).mkString("\n").indent(1),
         "}"
       ).mkString("\n"),
-
       "",
       Seq(
         s"func ${publicName}FromJson(bytes ${io}.Reader) $publicName {",
+        (Seq(
+          s"var $privateName $publicName"
+        ) ++
+        defaults ++
         Seq(
-          s"var $privateName $publicName",
           s"${json}.NewDecoder(bytes).Decode(&$privateName)",
           s"return $privateName"
-        ).mkString("\n").indent(1),
+        )).mkString("\n").indent(1),
         "}"
       ).mkString("\n")
 
@@ -220,7 +231,7 @@ case class Code(form: InvocationForm) {
           case Some(discriminator) => {
             Seq(
 	      s"var el map[string]interface{}",
-	      s"${json}.NewDecoder(bytes).Decode(&el)",
+	      s"${json}.Unmarshallbytes).Decode(&el)",
               s"""switch el["$discriminator"] {""",
 
               (
@@ -554,7 +565,7 @@ case class Code(form: InvocationForm) {
               case fields => {
                 Some(
                   fields.map { field =>
-                    val fieldGoType = GoType(importBuilder, datatype(field.`type`, field.required))
+                    val fieldGoType = GoType(importBuilder, datatype(field.`type`, field.required || field.default.isDefined))
                     val fieldName = importBuilder.publicName(field.name)
                     val fullName = s"${varName}.$fieldName"
 

go-generator/src/main/scala/models/Defaults.scala
@@ -1,0 +1,81 @@
+package go.models
+
+import java.time.LocalDate
+
+import go.models.ImportBuilder
+import io.apibuilder.spec.v0.models.{Field, Service}
+import lib.Datatype
+import org.joda.time.format.ISODateTimeFormat
+import org.joda.time.format.ISODateTimeFormat.dateTimeParser
+import play.api.libs.json._
+
+object Defaults {
+
+  /**
+    * Returns a default for a field if
+    * - a default exists
+    * - the default can be parsed into a Golang type
+    *
+    * The string returned should be ready to include on the right side of an assignment. Examples are
+    * true
+    * "hello world"
+    * 7.85
+    * string[]
+    *
+    */
+  def mkGolangDefault(default: Option[String], dt: Datatype, svc: Service,  importBuilder: ImportBuilder): Option[String] = {
+    default match {
+      case Some(defaultText) => {
+        dt match {
+          case Datatype.Primitive.Boolean => Some(wrapInQuotes(defaultText))
+          case Datatype.Primitive.Decimal => Some(toBigDecimal(Json.parse(defaultText)).toString)
+          case Datatype.Primitive.Integer => Some(toBigDecimal(Json.parse(defaultText)).toInt.toString)
+          case Datatype.Primitive.Double => Some(toBigDecimal(Json.parse(defaultText)).toDouble.toString)
+          case Datatype.Primitive.Long => Some(toBigDecimal(Json.parse(defaultText)).toLong.toString)
+          case Datatype.Primitive.String => Some(wrapInQuotes(defaultText))
+          case Datatype.Primitive.Uuid => Some(wrapInQuotes(defaultText))
+          case Datatype.Primitive.DateTimeIso8601 => Some(wrapInQuotes(defaultText))
+          case Datatype.Primitive.DateIso8601 => Some(wrapInQuotes(defaultText))
+          case Datatype.UserDefined.Enum(enumName) => {
+              svc.enums.find(_.name == enumName).flatMap(e=> e.values.find(_.name == defaultText).map(v=> importBuilder.publicName(enumName) + importBuilder.publicName(v.name)))
+          }
+          case Datatype.Container.List(t) => {
+            val arr = Json.parse(defaultText).as[JsArray]
+            val seq = arr.value.map { value =>
+              mkGolangDefault(Some(value.toString), t, svc, importBuilder)
+            }
+            if (seq.isEmpty) {
+              None
+            } else {
+              Some(s"[]string{${seq.collect{case Some(x)=>x}.mkString(",")}}")
+            }
+          }
+          case _ => None
+        }
+      }
+      case None => None
+    }
+  }
+
+  private[this] def toBigDecimal(json: JsValue): BigDecimal = {
+    json match {
+      case v: JsString => {
+        if (v.value.matches("0+")) {
+          BigDecimal("0")
+        } else {
+          BigDecimal(json.toString)
+        }
+      }
+      case v: JsNumber => v.value
+      case _ => BigDecimal(json.toString)
+    }
+  }
+
+  private[this] def wrapInQuotes(value: String): String = {
+    val inQuotesAlready = "\".*\"".r
+    value match {
+      case inQuotesAlready() => value
+      case _ => s""""$value""""
+    }
+  }
+}

go-generator/src/test/scala/models/GoReceiverDefaultsSpec.scala
@@ -1,0 +1,130 @@
+package models
+
+import go.models.GoClientGenerator
+import io.apibuilder.generator.v0.models.InvocationForm
+import org.scalatest.{FunSpec, Matchers}
+
+import scala.io.Source
+
+class GoReceiverDefaultsSpec extends FunSpec with Matchers {
+  it("Should produce models with defaults") {
+    val json = models.TestHelper.buildJson(
+      """
+      "imports": [],
+      "headers": [],
+      "info": [],
+      "unions": [],
+      "resources": [],
+      "attributes": [],
+      "enums": [{
+          "name": "car_type",
+          "plural": "car_types",
+          "values": [{
+              "name": "sedan",
+              "attributes": [],
+              "description": "most common"
+          }, {
+              "name": "coupe",
+              "attributes": []
+          }],
+          "attributes": [],
+          "description": "on has originated"
+      }],
+      "models": [{
+          "name": "model",
+          "plural": "models",
+          "fields": [{
+              "name": "guid",
+              "type": "uuid",
+              "required": false,
+              "default" : "abcd-ef01-2345-6789-abcd",
+              "attributes": []
+          }, {
+              "name": "name",
+              "type": "string",
+              "required": false,
+              "default": "M3",
+              "attributes": []
+          }, {
+              "name": "type",
+              "type": "car_type",
+              "required": false,
+              "default": "coupe",
+              "attributes": []
+          }, {
+              "name": "curb_weight",
+              "type": "integer",
+              "required": false,
+              "default": "3500",
+              "attributes": []
+          },{
+              "name": "serial",
+              "type": "long",
+              "required": false,
+              "default": "45678901234",
+              "attributes": []
+          }, {
+              "name": "final_drive",
+              "type": "double",
+              "required": false,
+              "default": "3.85",
+              "attributes": []
+          }, {
+              "name": "msrp",
+              "type": "decimal",
+              "required": false,
+              "default": "45999.99",
+              "attributes": []
+          }, {
+              "name": "is_flashy",
+              "type": "boolean",
+              "required": false,
+              "default": "true",
+              "attributes": []
+          },{
+              "name": "markets",
+              "type": "[string]",
+              "required": false,
+              "default": "[\"USA\",\"CAN\"]",
+              "attributes": []
+          },{
+              "name": "launched_on",
+              "type": "date-iso8601",
+              "required": false,
+              "default": "1986-02-01",
+              "attributes": []
+          },{
+              "name": "timestamp",
+              "type": "date-time-iso8601",
+              "required": false,
+              "default": "2018-03-21T02:20:52+00:00",
+              "attributes": []
+          }],
+          "attributes": [],
+          "description": "Model of a car."
+      }],
+      "resources": []
+      """)
+
+    val form = InvocationForm(models.TestHelper.service(json.format()))
+
+    val Right(res) = GoClientGenerator.invoke(form)
+
+    res.head.contents.contains("""func ModelFromJson(bytes io.Reader) Model {
+                   |	var model Model
+                   |	model.Guid = "abcd-ef01-2345-6789-abcd"
+                   |	model.Name = "M3"
+                   |	model.Type = CarTypeCoupe
+                   |	model.CurbWeight = 3500
+                   |	model.Serial = 45678901234
+                   |	model.FinalDrive = 3.85
+                   |	model.Msrp = 45999.99
+                   |	model.IsFlashy = "true"
+                   |	model.Markets = []string{"USA","CAN"}
+                   |	model.LaunchedOn = "1986-02-01"
+                   |	model.Timestamp = "2018-03-21T02:20:52+00:00"
+                   |	json.NewDecoder(bytes).Decode(&model)
+                   |	return model
+                   |}""".stripMargin) should be(true)
+  }
+}

commit 0cf4004ab5c8795150b8ea5f41e42ee56c19a9f3

fixed wrong backtick removal placement introduced in a67d474

scala-generator/src/main/scala/models/http4s/CirceJson.scala
@@ -176,9 +176,9 @@ ${Seq(generateEnums(), generateModels(), generateUnions()).filter(!_.isEmpty).mk
         .fields
         .map {field =>
           if (field.shouldApplyDefaultOnRead){
-            s"""${nobt(field.name)} = ${field.name}.getOrElse(${field.default.get})"""
+            s"""${field.name} = ${nobt(field.name)}.getOrElse(${field.default.get})"""
           } else {
-            s"""${nobt(field.name)} = ${field.name}"""
+            s"""${field.name} = ${nobt(field.name)}"""
           }
       }.mkString(",\n").indent(6),
       s"    )",