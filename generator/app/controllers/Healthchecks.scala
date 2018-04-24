7a5a5f3abed2b82f8e213637654a3bb2ec389090
Jakub Kozłowski <kubukoz@gmail.com>
2018-04-24 20:41:44 +0200

    Stop extending play.api.mvc.Controller directly

--- generator/app/controllers/Healthchecks.scala
+++ generator/app/controllers/Healthchecks.scala
@@ -4,11 +4,11 @@ import play.api._
 import play.api.mvc._
 import play.api.libs.json._
 
-class Healthchecks extends Controller {
+class Healthchecks extends InjectedController {
 
   private[this] val Result = Json.toJson(Map("status" -> "healthy"))
 
-  def get() = Action { request =>
+  def get() = Action {
     Ok(Result)
   }
 
