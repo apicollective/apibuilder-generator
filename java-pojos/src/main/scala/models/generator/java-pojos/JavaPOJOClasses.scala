9dc37786e99d91a71fa89fa86da59f97a7a21b9c
Daniel Kirby <dkirby@gilt.com>
2018-07-31 17:58:08 -0400

    Corrected naming.

--- java-pojos/src/main/scala/models/generator/java-pojos/JavaPOJOClasses.scala
+++ java-pojos/src/main/scala/models/generator/java-pojos/JavaPOJOClasses.scala
@@ -2,8 +2,8 @@ package models.generator.javapojos
 
 object JavaPOJOClasses
   extends BaseJavaPOJOCodeGenerator {
-  override def getJavaDocFileHeader() = "WARNING: not all features (notably unions) and data types work with android generator yet.  \n" +
-    "Android generator is designed to be used in an android application, but should work in any java codebase as long as you import jackson and retrofit2 libraries.  \n" +
-    "If you are considering using this library, would like to request/discuss features, or would like to share how you're using it, please contact android-feedback@gilt.com \n"
+  override def getJavaDocFileHeader() = "WARNING: not all features (notably unions) and data types work with the java generator yet.  \n" +
+    "Java POJO generator is designed to be used with AWS Lambdas.  \n" +
+    "If you are considering using this library, would like to request/discuss features, or would like to share how you're using it, please contact daniel.kirby@gilt.com \n"
 
 }
