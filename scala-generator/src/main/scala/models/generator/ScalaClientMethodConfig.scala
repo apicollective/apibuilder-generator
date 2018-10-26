558bd84a2730f66937591fd4d0f9a3294837a188
ibezrukavyi <36413415+ibezrukavyi@users.noreply.github.com>
2018-10-26 18:27:53 +0100

    Http4s 019 support remove deprecated Message.withEntity (#474)
    * initial commit
    - just copy paste everything from 0.18
    
    * replace deprecated HttpService with HttpRoutes
    
    * address comments
    
    * replace deprecated Message.withBody by Message.withEntity
    
    * fix issues
    
    * fix merge issues
    
    * fix merge issue

--- scala-generator/src/main/scala/models/generator/ScalaClientMethodConfig.scala
+++ scala-generator/src/main/scala/models/generator/ScalaClientMethodConfig.scala
@@ -221,6 +221,7 @@ private lazy val defaultAsyncHttpClient = PooledHttp1Client()
     def monadTransformerInvoke: String
     def asyncFailure: String
     def requestClass: String = "org.http4s.Request"
+    def requestType:  String = s"$asyncType[$requestClass]"
     def messageClass: String = "org.http4s.Message"
     def httpServiceClass: String = "org.http4s.HttpService"
     def generateDecodeResult(datatypeName: String): String = s"org.http4s.DecodeResult[$datatypeName]"
@@ -238,6 +239,11 @@ private lazy val defaultAsyncHttpClient = PooledHttp1Client()
                                              |def closeAsyncHttpClient(): Unit = {
                                              |  asyncHttpClient.shutdownNow()
                                              |}""".stripMargin)
+
+    def reqAndMaybeAuthAndBody: String = """val reqAndMaybeAuthAndBody = if (formBody.nonEmpty) {
+                                            |formBody.fold(Sync[F].pure(reqAndMaybeAuth))(reqAndMaybeAuth.withBody)
+                                            |} else body.fold(Sync[F].pure(reqAndMaybeAuth))(reqAndMaybeAuth.withBody)""".stripMargin
+
     def matchersImport: String
     def httpClient: String
   }
@@ -323,6 +329,7 @@ implicit def circeJsonDecoder[${asyncTypeParam(Some("Sync")).map(_+", ").getOrEl
     override val asyncSuccess: String = "pure"
     override def asyncFailure: String = "raiseError"
     override def requestClass: String = s"org.http4s.Request[$asyncType]"
+    override def requestType:  String = requestClass
     override def generateDecodeResult(datatypeName: String): String = s"org.http4s.DecodeResult[$asyncType, $datatypeName]"
     override def messageClass: String = s"org.http4s.Message[$asyncType]"
     override def httpServiceClass: String = s"org.http4s.HttpRoutes.of[$asyncType]"
@@ -351,5 +358,9 @@ implicit def circeJsonDecoder[${asyncTypeParam(Some("Sync")).map(_+", ").getOrEl
     override val httpClient: String = "httpClient"
 
     override val asyncTypeImport: String = "import cats.effect._"
+
+    override def reqAndMaybeAuthAndBody: String = """val reqAndMaybeAuthAndBody = if (formBody.nonEmpty) {
+                                           |formBody.fold(reqAndMaybeAuth)(reqAndMaybeAuth.withEntity)
+                                           |} else body.fold(reqAndMaybeAuth)(reqAndMaybeAuth.withEntity)""".stripMargin
   }
 }
