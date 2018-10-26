f7754d0a0a45e5d89cbf9237113870c340cefa07
Ievgen Bezrukavyi <ievgen.bezrukavyi@hbc.com>
2018-10-26 17:54:29 +0100

    fix issues

--- scala-generator/src/main/scala/models/generator/ScalaClientMethodConfig.scala
+++ scala-generator/src/main/scala/models/generator/ScalaClientMethodConfig.scala
@@ -233,10 +233,9 @@ private lazy val defaultAsyncHttpClient = PooledHttp1Client()
                                              |  asyncHttpClient.shutdownNow()
                                              |}""".stripMargin)
 
-    def reqAndMaybeAuthAndBody: String = """
-                                            |val reqAndMaybeAuthAndBody = if (formBody.nonEmpty) {
+    def reqAndMaybeAuthAndBody: String = """val reqAndMaybeAuthAndBody = if (formBody.nonEmpty) {
                                             |formBody.fold(Sync[F].pure(reqAndMaybeAuth))(reqAndMaybeAuth.withBody)
-                                            |} else body.fold(Sync[F].pure(reqAndMaybeAuth))(reqAndMaybeAuth.withBody)"""
+                                            |} else body.fold(Sync[F].pure(reqAndMaybeAuth))(reqAndMaybeAuth.withBody)""".stripMargin
 
     def matchersImport: String
     def httpClient: String
@@ -351,9 +350,8 @@ implicit def circeJsonDecoder[${asyncTypeParam(Some("Sync")).map(_+", ").getOrEl
 
     override val asyncTypeImport: String = "import cats.effect._"
 
-    override def reqAndMaybeAuthAndBody: String = """
-                                           |val reqAndMaybeAuthAndBody = if (formBody.nonEmpty) {
+    override def reqAndMaybeAuthAndBody: String = """val reqAndMaybeAuthAndBody = if (formBody.nonEmpty) {
                                            |formBody.fold(reqAndMaybeAuth)(reqAndMaybeAuth.withEntity)
-                                           |} else body.fold(reqAndMaybeAuth)(reqAndMaybeAuth.withEntity)"""
+                                           |} else body.fold(reqAndMaybeAuth)(reqAndMaybeAuth.withEntity)""".stripMargin
   }
 }
