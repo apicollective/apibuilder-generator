d1e8a317645c05c409866393f594f81afa9070c1
Michael Bryzek <mbryzek@alum.mit.edu>
2018-12-21 09:42:31 -0500

    Single quote url in curl debug command (#482)
    * Single quote url in curl debug command
    
      - Fixes bug when url has query params

--- scala-generator/src/main/scala/models/Play2ClientGenerator.scala
+++ scala-generator/src/main/scala/models/Play2ClientGenerator.scala
@@ -200,8 +200,8 @@ ${if (version.config.expectsInjectedWsClient) "" else "      import play.api.Pla
         value <- values
       } yield s"$$name=$$value"
       val url = s"$${req.url}$${queryComponents.mkString("?", "&", "")}"
-      auth.fold(logger.info(s"curl -X $$method $$url")) { _ =>
-        logger.info(s"curl -X $$method -u '[REDACTED]:' $$url")
+      auth.fold(logger.info(s"curl -X $$method '$$url'")) { _ =>
+        logger.info(s"curl -X $$method -u '[REDACTED]:' '$$url'")
       }
       req
     }
