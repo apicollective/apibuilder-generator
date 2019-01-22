414ad636f10ad5a797c90397c5fea4d937c5949c
Philippe Vinchon <p.vinchon@gmail.com>
2019-01-22 11:45:13 +0000

    Generate client without dependencies

--- scala-generator/src/main/scala/models/play/files/Client.scala
+++ scala-generator/src/main/scala/models/play/files/Client.scala
@@ -3,14 +3,36 @@ package scala.models.play.files
 import io.apibuilder.generator.v0.models.{File, InvocationForm}
 
 object Client {
-    def apply(form: InvocationForm): Either[Seq[String], File] =
-        scala.models.Play26ClientGenerator
-            .invoke(form)
-            .flatMap { files =>
-                files.headOption
-                    .toRight {
-                        val error = s"Service[${form.service.organization.key}/${form.service.application.key}] cannot generate play client"
-                        Seq(error)
-                    }
-            }
+    def contents(form: InvocationForm): String = {
+        val header = scala.models.ApidocComments(form.service.version, form.userAgent)
+
+        val gen = scala.models.Play2ClientGenerator(
+            scala.models.Play26ClientGenerator.config(form),
+            form
+        )
+
+        s"""
+            ${header.toJavaString()}
+
+            ${gen.client()}
+        """
+    }
+
+    def file(form: InvocationForm, contents: String): File =
+        generator.ServiceFileNames.toFile(
+            form.service.namespace,
+            form.service.organization.key,
+            form.service.application.key,
+            form.service.version,
+            "Client",
+            contents,
+            Some("Scala")
+        )
+
+    def apply(form: InvocationForm): Either[Seq[String], File] = {
+        val contents = this.contents(form)
+        val file = this.file(form, contents)
+
+        Right(file)
+    }
 }
