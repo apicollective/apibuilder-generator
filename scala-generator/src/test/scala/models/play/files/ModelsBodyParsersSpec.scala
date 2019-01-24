b0be2cdda509739dfa9b067c4a5b3f737961e461
Philippe Vinchon <p.vinchon@gmail.com>
2019-01-24 16:05:58 +0000

    Use scalacheck instead of hard coded types

--- scala-generator/src/test/scala/models/play/files/ModelsBodyParsersSpec.scala
+++ scala-generator/src/test/scala/models/play/files/ModelsBodyParsersSpec.scala
@@ -1,11 +1,17 @@
 package scala.models.play.files
 
 import io.apibuilder.spec.v0.models.{Enum, Model, Service, Union}
+import io.apibuilder.spec.v0.models.gens._
 import org.scalatest.{FunSpec, Matchers}
+import org.scalatest.prop.PropertyChecks
+import scala.models.play.gens._
 import scala.models.play.Helpers.compareWithoutWhiteSpaces
 import scala.generator.{ScalaEnum, ScalaModel, ScalaService, ScalaUnion}
 
-class ModelsBodyParsersSpec extends FunSpec with Matchers {
+class ModelsBodyParsersSpec extends FunSpec with Matchers with PropertyChecks {
+
+  implicit val scalacheckConfig = generatorDrivenConfig.copy(sizeRange = 10)
+
   it("generates generic body parser") {
     val expected = s"""
       private def bodyParser[A](parser: ${ModelsBodyParsers.BodyParser}[${ModelsBodyParsers.JsValue}])(implicit ec: ${ModelsBodyParsers.ExecutionContext}, rds: ${ModelsBodyParsers.JsReads}[A]): ${ModelsBodyParsers.BodyParser}[A] =
@@ -13,43 +19,45 @@ class ModelsBodyParsersSpec extends FunSpec with Matchers {
     """
 
     val result = ModelsBodyParsers.bodyParser()
-    compareWithoutWhiteSpaces(expected, result)
+    compareWithoutWhiteSpaces(result, expected)
   }
 
   it("generates body parser given a name, and type") {
-    val name = "Foo"
-    val tpe = "Bar"
-    val expected = s"""
-      def bodyParser${name}(parser: ${ModelsBodyParsers.BodyParser}[${ModelsBodyParsers.JsValue}])(implicit ec: ${ModelsBodyParsers.ExecutionContext}, rds: ${ModelsBodyParsers.JsReads}[${tpe}]): ${ModelsBodyParsers.BodyParser}[${tpe}] =
-        bodyParser[${tpe}](parser)
-    """
+    forAll { (name: String, tpe: String) =>
+      val expected = s"""
+        def bodyParser${name}(parser: ${ModelsBodyParsers.BodyParser}[${ModelsBodyParsers.JsValue}])(implicit ec: ${ModelsBodyParsers.ExecutionContext}, rds: ${ModelsBodyParsers.JsReads}[${tpe}]): ${ModelsBodyParsers.BodyParser}[${tpe}] =
+          bodyParser[${tpe}](parser)
+      """
 
-    val result = ModelsBodyParsers.bodyParser(name, tpe)
-
-    compareWithoutWhiteSpaces(expected, result)
+      val result = ModelsBodyParsers.bodyParser(name, tpe)
+      compareWithoutWhiteSpaces(result, expected)
+    }
   }
 
   it("generates body parser for enum based on name, and qualifiedName") {
-    val enum = new ScalaEnum(ScalaService(Service(null, "", null, null, "foo.bar", null, info = null)), Enum("Foo", null, values = Seq.empty))
-    val expected = ModelsBodyParsers.bodyParser(enum)
-    val result = ModelsBodyParsers.bodyParser(enum.name, enum.qualifiedName)
+    forAll { enum: ScalaEnum =>
+      val expected = ModelsBodyParsers.bodyParser(enum)
+      val result = ModelsBodyParsers.bodyParser(enum.name, enum.qualifiedName)
 
-    compareWithoutWhiteSpaces(expected, result)
+      compareWithoutWhiteSpaces(result, expected)
+    }
   }
 
   it("generates body parser for model based on name, and qualifiedName") {
-    val model = new ScalaModel(ScalaService(Service(null, "", null, null, "foo.bar", null, info = null)), Model("Foo", "Foos", fields = Seq.empty))
-    val expected = ModelsBodyParsers.bodyParser(model)
-    val result = ModelsBodyParsers.bodyParser(model.name, model.qualifiedName)
+    forAll { model: ScalaModel =>
+      val expected = ModelsBodyParsers.bodyParser(model)
+      val result = ModelsBodyParsers.bodyParser(model.name, model.qualifiedName)
 
-    compareWithoutWhiteSpaces(expected, result)
+      compareWithoutWhiteSpaces(result, expected)
+    }
   }
 
   it("generates body parser for union based on name, and qualifiedName") {
-    val union = new ScalaUnion(ScalaService(Service(null, "", null, null, "foo.bar", null, info = null)), Union("Foo", null, types = Seq.empty))
-    val expected = ModelsBodyParsers.bodyParser(union)
-    val result = ModelsBodyParsers.bodyParser(union.name, union.qualifiedName)
+    forAll { union: ScalaUnion =>
+      val expected = ModelsBodyParsers.bodyParser(union)
+      val result = ModelsBodyParsers.bodyParser(union.name, union.qualifiedName)
 
-    compareWithoutWhiteSpaces(expected, result)
+      compareWithoutWhiteSpaces(result, expected)
+    }
   }
 }
