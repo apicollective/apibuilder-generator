b2292678b5f2a4ca961e04e0882846a3086ea7e4
Adam Nadoba <adam@scalac.io>
2019-01-29 16:06:37 +0100

    fail if service.imports are not resolved

--- postman-generator/src/main/scala/postman/generator/PostmanCollectionGenerator.scala
+++ postman-generator/src/main/scala/postman/generator/PostmanCollectionGenerator.scala
@@ -1,11 +1,21 @@
 package postman.generator
 
 import io.apibuilder.generator.v0.models.{File, InvocationForm}
+import io.apibuilder.spec.v0.models.Service
 import lib.generator.CodeGenerator
 
 object PostmanCollectionGenerator extends CodeGenerator {
 
   override def invoke(form: InvocationForm): Either[Seq[String], Seq[File]] = {
+    form.importedServices match {
+      case None if form.service.imports.nonEmpty =>
+        Left(Seq("Service imports need to be resolved before generating Postman Collection. However InvocationForm.importedServices is empty"))
+      case None => invokeGenerator(form.service, Seq.empty)
+      case Some(importedServices) => invokeGenerator(form.service, importedServices)
+    }
+  }
+
+  private def invokeGenerator(service: Service, importedServices: Seq[Service]): Either[Seq[String], Seq[File]] = {
     Left(
       Seq("Not implemented yet!")
     )
