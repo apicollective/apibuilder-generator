b32751bff68858b817dde6d0a060c3b4854f00d3
Adam Nadoba <adam@scalac.io>
2019-01-30 13:45:37 +0100

    add postman generator heuristics along with its unit tests

---
+++ postman-generator/src/main/scala/generator/Heuristics.scala
@@ -0,0 +1,34 @@
+package generator
+
+
+import io.apibuilder.spec.v0.models.Resource
+
+
+object Heuristics {
+
+  case class PathVariable(name: String, postmanVarName: String)
+
+  val IdFieldHeuristicThreshold = 3
+
+  def idFieldHeuristic(resource: Resource): Option[String] = {
+    val pathsWithOperation = resource.operations.flatMap { operation =>
+      findPathParams(operation.path)
+        .map(param => (param, resource))
+    }
+    pathsWithOperation
+      .groupBy{case (param, _) => param}
+      .mapValues(_.size)
+      .filter(_._1 != "organization")
+      .filter(_._2 >= IdFieldHeuristicThreshold)
+      .map(_._1)
+      .headOption
+  }
+
+  def findPathParams(path: String): Seq[String] = {
+    val regex = """\:(\w+)[\/]{0,1}""".r
+    regex
+      .findAllIn(path)
+      .map(str => regex.replaceAllIn(str, "$1"))
+      .toList
+  }
+}
