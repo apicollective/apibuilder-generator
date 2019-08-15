d264705a03de004398750efc0d0753584433385f
Sean Sullivan <github@seansullivan.com>
2019-08-15 02:07:45 -0400

    improve [NamespacesSpec.scala]

--- scala-generator/src/test/scala/models/generator/NamespacesSpec.scala
+++ scala-generator/src/test/scala/models/generator/NamespacesSpec.scala
@@ -1,6 +1,5 @@
 package scala.generator
 
-import io.apibuilder.spec.v0.models.Method
 import org.scalatest.{Matchers, FunSpec}
 
 class NamespacesSpec extends FunSpec with Matchers {
@@ -8,6 +7,10 @@ class NamespacesSpec extends FunSpec with Matchers {
   it("quotes keywords") {
     val ns = Namespaces("io.apibuilder.example.union.type.v0")
     ns.base should be("io.apibuilder.example.union.`type`.v0")
+    ns.enums should be ("io.apibuilder.example.union.`type`.v0.models")
+    ns.models should be ("io.apibuilder.example.union.`type`.v0.models")
+    ns.unions should be ("io.apibuilder.example.union.`type`.v0.models")
+    ns.mock should be ("io.apibuilder.example.union.`type`.v0.mock")
   }
 
 }
