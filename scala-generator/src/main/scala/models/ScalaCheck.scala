ae0ed08e65975a371f9ac63b118be000a1671af1
Philippe Vinchon <p.vinchon@gmail.com>
2020-01-10 10:58:47 +0000

    Remove manual formatting

--- scala-generator/src/main/scala/models/ScalaCheck.scala
+++ scala-generator/src/main/scala/models/ScalaCheck.scala
@@ -26,15 +26,15 @@ object ScalaCheckGenerator extends CodeGenerator {
 
   def model(namespace: String, model: ScalaModel): String = {
     def field(field: ScalaField): String = s"""
-      |${field.name} <- Arbitrary.arbitrary[${field.datatype.name}]
-    """.stripMargin.trim
+      ${field.name} <- Arbitrary.arbitrary[${field.datatype.name}]
+    """
 
     s"""
-      |implicit def arbitrary${model.name}: Arbitrary[${namespace}.${model.name}] = Arbitrary(gen${model.name})
-      |def gen${model.name}: Gen[${namespace}.${model.name}] = for {
-      |  ${model.fields.map(field).mkString("\n")}
-      |} yield ${namespace}.${model.name}(${model.fields.map(_.name).mkString(", ")})
-    """.stripMargin.trim
+      implicit def arbitrary${model.name}: Arbitrary[${namespace}.${model.name}] = Arbitrary(gen${model.name})
+      def gen${model.name}: Gen[${namespace}.${model.name}] = for {
+        ${model.fields.map(field).mkString("\n")}
+      } yield ${namespace}.${model.name}(${model.fields.map(_.name).mkString(", ")})
+    """
   }
 
   def enum(namespace: String, enum: ScalaEnum): String = {
@@ -46,9 +46,9 @@ object ScalaCheckGenerator extends CodeGenerator {
     }
 
     s"""
-      |implicit def arbitrary${enum.name}: Arbitrary[${namespace}.${enum.name}] = Arbitrary(gen${enum.name})
-      |def gen${enum.name}: Gen[${namespace}.${enum.name}] = ${gen}
-    """.stripMargin.trim
+      implicit def arbitrary${enum.name}: Arbitrary[${namespace}.${enum.name}] = Arbitrary(gen${enum.name})
+      def gen${enum.name}: Gen[${namespace}.${enum.name}] = ${gen}
+    """
   }
 
   def union(namespace: String, union: ScalaUnion): String = {
@@ -60,47 +60,47 @@ object ScalaCheckGenerator extends CodeGenerator {
     }
 
     s"""
-      |implicit def arbitrary${union.name}: Arbitrary[${namespace}.${union.name}] = Arbitrary(gen${union.name})
-      |def gen${union.name}: Gen[${namespace}.${union.name}] = ${gen}
-    """.stripMargin.trim
+      implicit def arbitrary${union.name}: Arbitrary[${namespace}.${union.name}] = Arbitrary(gen${union.name})
+      def gen${union.name}: Gen[${namespace}.${union.name}] = ${gen}
+    """
   }
 
   def abstractArbitrary(): String = s"""
-    |implicit def arbitraryJodaDateTime: Arbitrary[_root_.org.joda.time.DateTime]
-    |implicit def arbitraryPlayJsObject: Arbitrary[_root_.play.api.libs.json.JsObject]
-  """.stripMargin.trim
+    implicit def arbitraryJodaDateTime: Arbitrary[_root_.org.joda.time.DateTime]
+    implicit def arbitraryPlayJsObject: Arbitrary[_root_.play.api.libs.json.JsObject]
+  """
 
   def abstractArbitraryImplementation(): String = s"""
-    |implicit def arbitraryJodaDateTime: Arbitrary[_root_.org.joda.time.DateTime] = Arbitrary(genJodaDateTime)
-    |def genJodaDateTime: Gen[_root_.org.joda.time.DateTime] = Gen.posNum[Long].map(instant => new _root_.org.joda.time.DateTime(instant))
-    |
-    |implicit def arbitraryPlayJsObject: Arbitrary[_root_.play.api.libs.json.JsObject] = Arbitrary(genPlayJsObject)
-    |def genPlayJsObject: Gen[_root_.play.api.libs.json.JsObject] = Gen.const(_root_.play.api.libs.json.JsObject.empty)
-  """.stripMargin.trim
+    implicit def arbitraryJodaDateTime: Arbitrary[_root_.org.joda.time.DateTime] = Arbitrary(genJodaDateTime)
+    def genJodaDateTime: Gen[_root_.org.joda.time.DateTime] = Gen.posNum[Long].map(instant => new _root_.org.joda.time.DateTime(instant))
+
+    implicit def arbitraryPlayJsObject: Arbitrary[_root_.play.api.libs.json.JsObject] = Arbitrary(genPlayJsObject)
+    def genPlayJsObject: Gen[_root_.play.api.libs.json.JsObject] = Gen.const(_root_.play.api.libs.json.JsObject.empty)
+  """
 
   def fileContents(form: InvocationForm, ssd: ScalaService): String =
     s"""
-      |${header(form)}
-      |package ${packageName(ssd.namespaces.base)}
-      |
-      |import org.scalacheck.{Arbitrary, Gen}
-      |
-      |object ${objectName(ssd.name)} extends ${traitName(ssd.name)} {
-      | ${abstractArbitraryImplementation}
-      |}
-      |
-      |trait ${traitName(ssd.name)} ${extendsWith(form.service.imports)} {
-      |
-      |  ${abstractArbitrary}
-      |
-      |  ${ssd.models.map(model(ssd.namespaces.models, _)).mkString("\n\n")}
-      |
-      |  ${ssd.enums.map(enum(ssd.namespaces.models,_)).mkString("\n\n")}
-      |
-      |  ${ssd.unions.map(union(ssd.namespaces.models, _)).mkString("\n\n")}
-      |
-      |}
-    """.stripMargin.trim
+      ${header(form)}
+      package ${packageName(ssd.namespaces.base)}
+
+      import org.scalacheck.{Arbitrary, Gen}
+
+      object ${objectName(ssd.name)} extends ${traitName(ssd.name)} {
+       ${abstractArbitraryImplementation}
+      }
+
+      trait ${traitName(ssd.name)} ${extendsWith(form.service.imports)} {
+
+        ${abstractArbitrary}
+
+        ${ssd.models.map(model(ssd.namespaces.models, _)).mkString("\n\n")}
+
+        ${ssd.enums.map(enum(ssd.namespaces.models,_)).mkString("\n\n")}
+
+        ${ssd.unions.map(union(ssd.namespaces.models, _)).mkString("\n\n")}
+
+      }
+    """
 
   override def invoke(form: InvocationForm): Either[Seq[String], Seq[File]] = {
     val ssd = ScalaService(form.service)
