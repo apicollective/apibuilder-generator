d92418582f53118e6735093c790c13d560582ad7
Philippe Vinchon <p.vinchon@gmail.com>
2020-01-16 13:25:45 +0000

    Play typed controller v2 (#581)
    * Use Inheritance over Composition
    
    * Update tests
    
    * Base controller only available from 2.6 and up
    
    * Add a bit of spacing

---
+++ scala-generator/src/main/scala/models/Play26Controllers.scala
@@ -0,0 +1,105 @@
+package scala.models
+
+import cats.implicits._
+import lib.Datatype
+import lib.generator.CodeGenerator
+import io.apibuilder.generator.v0.models.{File, InvocationForm}
+import io.apibuilder.spec.v0.models._
+import scala.generator._
+
+object Play26Controllers extends CodeGenerator {
+
+  def importJson(`import`: Import) =
+    s"import ${`import`.namespace}.models.json._"
+
+  def imports(ssd: ScalaService) =
+    s"""
+      import ${ssd.namespaces.json}._
+      ${ssd.service.imports.map(importJson).mkString("\n")}
+    """
+
+  def responseEnumName(operation: ScalaOperation) = s"${operation.name.capitalize}"
+
+  def responseObjectCode(response: ScalaResponse) = response.code match {
+    case ResponseCodeInt(code) => Some(code)
+    case _ => None
+  }
+
+  def responseObjectName(response: ScalaResponse) = responseObjectCode(response).map(code => s"HTTP${code}")
+
+  def responseObject(operation: ScalaOperation, response: ScalaResponse) = (responseObjectName(response), response.isUnit) match {
+    case (Some(name), true) => Some(s"case object ${name} extends ${responseEnumName(operation)}")
+    case (Some(name), false) => Some(s"case class ${name}(body: ${response.datatype.name}) extends ${responseEnumName(operation)}")
+    case _ => None
+  }
+
+  def responses(operation: ScalaOperation) =
+    s"""
+      sealed trait ${responseEnumName(operation)} extends Product with Serializable
+      object ${responseEnumName(operation)} {
+        ${operation.responses.flatMap(responseObject(operation, _)).mkString("\n")}
+      }
+    """
+
+  def responseToPlay(operation: ScalaOperation, response: ScalaResponse) =
+    (responseObjectName(response), responseObjectCode(response), response.isUnit) match {
+      case (Some(name), Some(code), true) => Some(s"case ${responseEnumName(operation)}.${name} => Status(${code})(play.api.mvc.Results.EmptyContent())")
+      case (Some(name), Some(code), false) => Some(s"case ${responseEnumName(operation)}.${name}(body) => Status(${code})(play.api.libs.json.Json.toJson(body))")
+      case _ => None
+    }
+
+  def controllerMethod(operation: ScalaOperation) = {
+    val bodyParser = operation.body.fold("")(body => s"(parse.json[${body.datatype.name}])")
+
+    val parameterNames =
+      List("request") ++
+      operation.parameters.map(_.name) ++
+      operation.body.map(_ => "request.body").toList
+
+    val parameterNameAndTypes =
+      List(s"""request: play.api.mvc.Request[${operation.body.fold("play.api.mvc.AnyContent")(_.datatype.name)}]""") ++
+      operation.parameters.map(p => s"${p.name}: ${p.datatype.name}") ++
+      operation.body.map(body => s"body: ${body.datatype.name}").toList
+
+    s"""
+      ${responses(operation)}
+
+      def ${operation.name}(${parameterNameAndTypes.mkString(", ")}): scala.concurrent.Future[${responseEnumName(operation)}]
+      final def ${operation.name}(${operation.parameters.map(p => s"${p.name}: ${p.datatype.name}").mkString(", ")}): play.api.mvc.Handler = Action.async${bodyParser} { request =>
+        ${operation.name}(${parameterNames.mkString(", ")})
+          .map {
+            ${operation.responses.flatMap(responseToPlay(operation, _)).mkString("\n")}
+          }(defaultExecutionContext)
+      }
+    """
+  }
+
+  def controller(resource: ScalaResource) =
+    s"""
+      trait ${resource.plural}Controller extends play.api.mvc.BaseController {
+        ${resource.operations.map(controllerMethod).mkString("\n\n")}
+      }
+    """
+
+  def controllers(resources: Seq[ScalaResource]) = resources.map(controller).mkString("\n\n")
+
+  def fileContents(form: InvocationForm, ssd: ScalaService): String =
+    s"""
+      ${ApidocComments(form.service.version, form.userAgent).toJavaString()}
+      package ${ssd.namespaces.base}.controllers
+
+      ${imports(ssd)}
+
+      ${controllers(ssd.resources)}
+    """
+
+  override def invoke(form: InvocationForm): Either[Seq[String], Seq[File]] = {
+    val ssd = ScalaService(form.service)
+    val name = s"${ssd.name.split('-').map(_.capitalize).mkString}Controllers.scala"
+    val contents = fileContents(form, ssd)
+
+    utils.ScalaFormatter.format(contents)
+      .map(contents => Seq(File(name, None, contents, None)))
+      .leftMap(t => Seq(t.getMessage))
+  }
+}
--- scala-generator/src/test/scala/models/Play26ControllersSpec.scala
+++ scala-generator/src/test/scala/models/Play26ControllersSpec.scala
@@ -6,10 +6,10 @@ import models.TestHelper._
 import org.scalatest.{FunSpec, Matchers}
 import scala.util.matching.Regex
 
-class Play2ControllersSpec extends FunSpec with Matchers {
+class Play26ControllersSpec extends FunSpec with Matchers {
 
   def fileContent(service: Service): File =
-    Play2Controllers.invoke(InvocationForm(service))
+    Play26Controllers.invoke(InvocationForm(service))
       .fold(
         { msgs => Left(new Throwable(s"Generated errors: ${msgs.mkString("\n  - ", "\n  - ", "")}")) },
         {
@@ -26,12 +26,11 @@ class Play2ControllersSpec extends FunSpec with Matchers {
   }
 
   def importCount(service: Service): Int = count("import ".r, service)
-  def responseCount(service: Service): Int = count("trait [^ ]+Responses".r, service)
+  def controllerCount(service: Service): Int = count("play\\.api\\.mvc\\.BaseController".r, service)
+  def responseCount(service: Service): Int = count("sealed trait [^ ]+".r, service)
   def responseImplementationCount(service: Service): Int = count("case (class|object) HTTP[0-9]+".r, service)
-  def serviceCount(service: Service): Int = count("trait [^ ]+Service".r, service)
-  def serviceMethodCount(service: Service): Int = count("scala\\.concurrent\\.Future".r, service)
-  def controllerCount(service: Service): Int = count("@javax\\.inject\\.Singleton".r, service)
-  def controllerMethodCount(service: Service): Int = count("Action\\.async".r, service)
+  def methodFinalCount(service: Service): Int = count("play\\.api\\.mvc\\.Handler".r, service)
+  def methodAbstractCount(service: Service): Int = count("scala\\.concurrent\\.Future".r, service)
 
   describe("for all services") {
     List(
@@ -39,7 +38,7 @@ class Play2ControllersSpec extends FunSpec with Matchers {
       referenceApiService,
       referenceWithImportsApiService,
       generatorApiService,
-      // apidocApiService,
+      apidocApiService,
       dateTimeService,
       builtInTypesService,
       generatorApiServiceWithUnionAndDescriminator,
@@ -51,6 +50,10 @@ class Play2ControllersSpec extends FunSpec with Matchers {
           assert(importCount(service) == 1 + service.imports.size)
         }
 
+        it("generates all controllers") {
+          assert(controllerCount(service) == service.resources.size)
+        }
+
         it("generates all responses") {
           assert(responseCount(service) == service.resources.flatMap(_.operations).size)
         }
@@ -59,20 +62,12 @@ class Play2ControllersSpec extends FunSpec with Matchers {
           assert(responseImplementationCount(service) == service.resources.flatMap(_.operations).flatMap(_.responses).size)
         }
 
-        it("generates all services") {
-          assert(serviceCount(service) == service.resources.size)
+        it("generates all methods final") {
+          assert(methodFinalCount(service) == service.resources.flatMap(_.operations).size)
         }
 
-        it("generates all service methods") {
-          assert(serviceMethodCount(service) == service.resources.flatMap(_.operations).size)
-        }
-
-        it("generates all controllers") {
-          assert(controllerCount(service) == service.resources.size)
-        }
-
-        it("generates all controller methods") {
-          assert(controllerMethodCount(service) == service.resources.flatMap(_.operations).size)
+        it("generates all methods abstract") {
+          assert(methodAbstractCount(service) == service.resources.flatMap(_.operations).size)
         }
       }
     }
