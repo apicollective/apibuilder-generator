bafa241d3192bfafa9e658f459d08dd6ea0baee5
Philippe Vinchon <p.vinchon@gmail.com>
2020-01-30 12:41:50 -0500

    Test for new elements

--- scala-generator/src/test/scala/models/Play26ControllersSpec.scala
+++ scala-generator/src/test/scala/models/Play26ControllersSpec.scala
@@ -26,9 +26,11 @@ class Play26ControllersSpec extends FunSpec with Matchers {
   }
 
   def importCount(service: Service): Int = count("import ".r, service)
-  def controllerCount(service: Service): Int = count("play\\.api\\.mvc\\.BaseController".r, service)
+  def executionContextCount(service: Service): Int = count("class [^ ]+ExecutionContext".r, service)
+  def responseTraitCount(service: Service): Int = count("trait [^ ]+Responses".r, service)
   def responseCount(service: Service): Int = count("sealed trait [^ ]+".r, service)
   def responseImplementationCount(service: Service): Int = count("case (class|object) HTTP[0-9]+".r, service)
+  def controllerCount(service: Service): Int = count("play\\.api\\.mvc\\.BaseController".r, service)
   def methodFinalCount(service: Service): Int = count("play\\.api\\.mvc\\.Action".r, service)
   def methodAbstractCount(service: Service): Int = count("scala\\.concurrent\\.Future".r, service)
 
@@ -50,8 +52,12 @@ class Play26ControllersSpec extends FunSpec with Matchers {
           assert(importCount(service) == 1 + service.imports.size)
         }
 
-        it("generates all controllers") {
-          assert(controllerCount(service) == service.resources.size)
+        it("generates all execution context") {
+          assert(executionContextCount(service) == service.resources.size)
+        }
+
+        it("generates all responses trait") {
+          assert(responseTraitCount(service) == service.resources.size)
         }
 
         it("generates all responses") {
@@ -60,6 +66,10 @@ class Play26ControllersSpec extends FunSpec with Matchers {
 
         it("generates all response implementations") {
           assert(responseImplementationCount(service) == service.resources.flatMap(_.operations).flatMap(_.responses).size)
+        }
+
+        it("generates all controllers") {
+          assert(controllerCount(service) == service.resources.size)
         }
 
         it("generates all methods final") {
