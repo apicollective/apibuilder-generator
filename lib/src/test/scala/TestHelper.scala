53ce2e297581f2d027db49fe575d83c344d86006
Michael Bryzek <mike@flow.io>
2020-05-01 12:03:30 -0400

    println

--- lib/src/test/scala/TestHelper.scala
+++ lib/src/test/scala/TestHelper.scala
@@ -150,12 +150,12 @@ object TestHelper extends Matchers {
       val expectedPath = "/tmp/apidoc.tmp.expected." + Text.safeName(filename)
       TestHelper.writeToFile(expectedPath, contents.trim)
       if (Files.exists(OverwriteTestsFile)) {
-        System.out.println(s"Overwriting test output as File $OverwriteTestsFile exists")
+        println(s"Overwriting test output as File $OverwriteTestsFile exists")
         TestHelper.writeToFile(actualPath, contents.trim)
       } else {
-        System.out.println("If you would like to update the test output automatically")
-        System.out.println(s"  1. touch $expectedPath")
-        System.out.println("  2. rerun the tests")
+        println("If you would like to update the test output automatically")
+        println(s"  1. touch $expectedPath")
+        println("  2. rerun the tests")
       }
 
       val cmd = s"diff $expectedPath $actualPath"
