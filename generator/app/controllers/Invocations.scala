5345abe03113513146ae63d40be7211165cfbe86
Michael Bryzek <mike@flow.io>
2021-08-16 12:44:03 +0000

    Debug HTTP 400

--- generator/app/controllers/Invocations.scala
+++ generator/app/controllers/Invocations.scala
@@ -8,23 +8,29 @@ import play.api.mvc._
 
 class Invocations extends InjectedController {
 
-  def postByKey(key: String): Action[JsValue] = Action(parse.json) { request =>
-      Generators.findGenerator(key).map(_.generator) match {
-        case None => NotFound
-        case Some(generator) =>
-          request.body.validate[InvocationForm] match {
-            case e: JsError => Conflict(Json.toJson(Validation.invalidJson(e)))
-            case s: JsSuccess[InvocationForm] => {
-              generator.invoke(s.get) match {
-                case Left(errors) => Conflict(Json.toJson(Validation.errors(errors)))
-                case Right(sourceFiles) =>
-                  // Also send back single source for backwards compatibility
-                  val singleSource = sourceFiles.map(_.contents).mkString("\n\n").trim
-                  Ok(Json.toJson(Invocation(singleSource, sourceFiles)))
+  def postByKey(key: String): Action[AnyContent] = Action { request =>
+    request.body.asJson match {
+      case None => Conflict(Json.toJson(Validation.error("Must provide form data (JSON)")))
+      case Some(js) => {
+        Generators.findGenerator(key).map(_.generator) match {
+          case Some(generator) =>
+            js.validate[InvocationForm] match {
+              case e: JsError => Conflict(Json.toJson(Validation.invalidJson(e)))
+              case s: JsSuccess[InvocationForm] => {
+                val form = s.get
+                generator.invoke(form) match {
+                  case Left(errors) => Conflict(Json.toJson(Validation.errors(errors)))
+                  case Right(sourceFiles) =>
+                    // Also send back single source for backwards compatibility
+                    val singleSource = sourceFiles.map(_.contents).mkString("\n\n").trim
+                    Ok(Json.toJson(Invocation(singleSource, sourceFiles)))
+                }
               }
             }
-          }
+          case _ => NotFound
+        }
       }
+    }
   }
 
 }
