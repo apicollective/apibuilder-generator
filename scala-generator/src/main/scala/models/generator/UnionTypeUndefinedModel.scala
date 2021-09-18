e5b5b1055e1f3c013e9e91f31024fcb307f7aeeb
Michael Bryzek <mike@flow.io>
2021-09-18 17:57:15 -0400

    Fix bug with unions and interfaces with field named 'description' (#649)

--- scala-generator/src/main/scala/models/generator/UnionTypeUndefinedModel.scala
+++ scala-generator/src/main/scala/models/generator/UnionTypeUndefinedModel.scala
@@ -1,35 +1,52 @@
 package scala.generator
 
 import lib.Datatype
-
 import io.apibuilder.spec.v0.models.{Field, Model}
+
+import scala.annotation.tailrec
 
 case class UnionTypeUndefinedModelWrapper(model: ScalaModel, union: ScalaUnion, interfaceFields: Seq[ScalaField])
 
 case class UnionTypeUndefinedModel(ssd: ScalaService) {
 
+  private[this] def descriptionField(name: String): Field = Field(
+    name = name,
+    description = Some("Information about the type that we received that is undefined in this version of the client."),
+    `type` = Datatype.Primitive.String.name,
+    required = true,
+  )
+
   val models: Seq[UnionTypeUndefinedModelWrapper] = ssd.unions.map { union =>
     // TODO: Verify another type does not exist with this name
+    val interfaceFields = ssd.findAllInterfaceFields(union.union.interfaces)
     val name = s"${union.name}UndefinedType"
     val model = Model(
       name = name,
       plural = s"${name}s",
       description = Some(s"Provides future compatibility in clients - in the future, when a type is added to the union ${union.name}, it will need to be handled in the client code. This implementation will deserialize these future types as an instance of this class."),
-      fields = Seq(
-        Field(
-          name = "description",
-          description = Some(s"Information about the type that we received that is undefined in this version of the client."),
-          `type` = Datatype.Primitive.String.name,
-          required = true
-        )
-      )
+      fields = buildFields(interfaceFields.map(_.name).toSet),
     )
+    println(s"Model fields: ${model.fields.map(_.name)}")
 
     UnionTypeUndefinedModelWrapper(
       model = new ScalaModel(ssd, model),
       union = union,
-      interfaceFields = ssd.findAllInterfaceFields(union.union.interfaces),
+      interfaceFields = interfaceFields,
     )
   }
 
+  @tailrec
+  private[this] def buildFields(interfaceFieldNames: Set[String], count: Int = 0): Seq[Field] = {
+    val finalName = count match {
+      case 0 => "description"
+      case 1 => "typeDescription"
+      case n => s"type${n}Description"
+    }
+
+    if (interfaceFieldNames.contains(finalName)) {
+      buildFields(interfaceFieldNames, count = count + 1)
+    } else {
+      Seq(descriptionField(finalName))
+    }
+  }
 }
---
+++ scala-generator/src/test/scala/models/UndefinedUnionWithInterfaceSpec.scala
@@ -0,0 +1,65 @@
+package models
+
+import io.apibuilder.generator.v0.models.InvocationForm
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+
+import scala.models.Play28ClientGenerator
+
+class UndefinedUnionWithInterfaceSpec extends AnyFunSpec with Matchers {
+
+  private[this] val json: String = models.TestHelper.buildJson("""
+      "imports": [],
+      "headers": [],
+      "info": [],
+      "enums": [],
+      "resources": [],
+      "attributes": [],
+
+      "unions": [
+        {
+          "name": "user",
+          "plural": "users",
+          "attributes": [],
+          "interfaces": ["user"],
+          "types": [
+            { "type": "registered_user", "attributes": [] }
+          ]
+        }
+      ],
+
+      "interfaces": [
+        {
+          "name": "user",
+          "plural": "user",
+          "attributes": [],
+          "fields": [
+            { "name": "description", "type": "string", "required": false, "attributes": [] }
+          ]
+        }
+      ],
+
+      "models": [
+        {
+          "name": "registered_user",
+          "plural": "registered_users",
+          "attributes": [],
+          "fields": [
+            { "name": "description", "type": "string", "required": false, "attributes": [] }
+          ]
+        }
+      ]
+  """)
+
+
+  it("codegen") {
+    val form = InvocationForm(
+      models.TestHelper.service(json),
+      attributes = Nil,
+      None
+    )
+    val Right(files) = Play28ClientGenerator.invoke(form)
+    models.TestHelper.assertEqualsFile("/undefined-union-with-interface-spec.txt", files.head.contents)
+  }
+
+}
