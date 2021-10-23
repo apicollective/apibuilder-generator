8a33aee9b3b15745f03fec81662b9015c844251e
Michael Bryzek <mike@flow.io>
2021-10-23 16:46:06 -0400

    wip

--- scala-generator/src/main/scala/models/Play26Controllers.scala
+++ scala-generator/src/main/scala/models/Play26Controllers.scala
@@ -43,12 +43,13 @@ object Play26Controllers extends CodeGenerator {
     responseObjectName: String,
     responseDatatype: Datatype,
     responseConfig: ResponseConfig,
+    code: Int,
   )
 
   private[this] def responseObject(responseObjectArgs: ResponseObjectArgs): String = {
     val args = ListBuffer[String]()
-    if (responseObjectArgs.responseDatatype == Datatype.Primitive.Unit) {
-      args.append(s"body: ${responseObjectArgs.responseDatatype}")
+    if (responseObjectArgs.responseDatatype != Datatype.Primitive.Unit) {
+      args.append(s"body: ${responseObjectArgs.responseDatatype.name}")
     }
     overrideHeaders(responseObjectArgs.responseConfig).foreach { code =>
       args.append(code)
@@ -69,7 +70,7 @@ object Play26Controllers extends CodeGenerator {
       .build
   }
 
-  private[this] def responses(operation: ScalaOperation, response: ResponseConfig) = {
+  private[this] def responses(operation: ScalaOperation, response: ResponseConfig, responseTypes: Seq[ResponseObjectArgs]) = {
     val name = responseEnumName(operation)
     val headersCode = response match {
       case ResponseConfig.Standard => ""
@@ -80,24 +81,6 @@ object Play26Controllers extends CodeGenerator {
            |""".stripMargin.strip
     }
 
-    val responseTypes = operation.responses.flatMap { r =>
-      responseObjectName(r).map { n =>
-        ResponseObjectArgs(
-          extendsClass = name,
-          responseObjectName = n,
-          responseDatatype = r.`type`,
-          responseConfig = response,
-        )
-      }
-    } ++ Seq(
-      ResponseObjectArgs(
-        extendsClass = name,
-        responseObjectName = "Undocumented",
-        responseDatatype = Datatype.Primitive.Unit,
-        responseConfig = response,
-      )
-    )
-
     s"""
       sealed trait $name extends Product with Serializable$headersCode
       object $name {
@@ -106,13 +89,22 @@ object Play26Controllers extends CodeGenerator {
     """
   }
 
-
-  private[this] def responseToPlay(operation: ScalaOperation, response: ScalaResponse): Option[String] =
+/*
+  private[this] def responseToPlayOrig(operation: ScalaOperation, response: ScalaResponse): Option[String] =
     (responseObjectName(response), responseObjectCode(response), response.isUnit) match {
-      case (Some(name), Some(code), true) => Some(s"case ${responseEnumName(operation)}.$name => Status($code)(play.api.mvc.Results.EmptyContent())")
-      case (Some(name), Some(code), false) => Some(s"case ${responseEnumName(operation)}.$name(body) => Status($code)(play.api.libs.json.Json.toJson(body))")
+      case (Some(name), Some(code), true) => Some(s"case r: ${responseEnumName(operation)}.$name => Status($code)(play.api.mvc.Results.EmptyContent())")
+      case (Some(name), Some(code), false) => Some(s"case r: ${responseEnumName(operation)}.$name => Status($code)(play.api.libs.json.Json.toJson(body))")
       case _ => None
     }
+ */
+
+  private[this] def responseToPlay(args: ResponseObjectArgs): String = {
+    if (args.responseDatatype == Datatype.Primitive.Unit) {
+      s"case ${args.extendsClass}.${args.responseObjectName} => Status(${args.code})(play.api.mvc.Results.EmptyContent())"
+    } else {
+      s"case r: ${args.extendsClass}.${args.responseObjectName} => Status(${args.code})(play.api.libs.json.Json.toJson(body))"
+    }
+  }
 
   private[this] def controllerMethod(operation: ScalaOperation, response: ResponseConfig): String = {
     val bodyType = operation.body.fold("play.api.mvc.AnyContent")(_.datatype.name)
@@ -128,14 +120,37 @@ object Play26Controllers extends CodeGenerator {
       operation.parameters.map(p => s"${ScalaUtil.quoteNameIfKeyword(p.name)}: ${p.datatype.name}") ++
       operation.body.map(body => s"body: ${body.datatype.name}").toList
 
+    val name = responseEnumName(operation)
+    val responseTypes = operation.responses.flatMap { r =>
+      responseObjectName(r).flatMap { n =>
+        responseObjectCode(r).map { code =>
+          ResponseObjectArgs(
+            extendsClass = name,
+            responseObjectName = n,
+            responseDatatype = r.`type`,
+            responseConfig = response,
+            code = code,
+          )
+        }
+      }
+    } ++ Seq(
+      ResponseObjectArgs(
+        extendsClass = name,
+        responseObjectName = "Undocumented",
+        responseDatatype = Datatype.Primitive.Unit,
+        responseConfig = response,
+        code = 500,
+      )
+    )
+
     s"""
-      ${responses(operation, response)}
+      ${responses(operation, response, responseTypes)}
 
       def ${operation.name}(${parameterNameAndTypes.mkString(", ")}): scala.concurrent.Future[${responseEnumName(operation)}]
       final def ${operation.name}(${operation.parameters.map(p => s"${ScalaUtil.quoteNameIfKeyword(p.name)}: ${p.datatype.name}").mkString(", ")}): play.api.mvc.Action[$bodyType] = Action.async$bodyParser { request =>
         ${operation.name}(${parameterNames.mkString(", ")})
           .map {
-            ${operation.responses.flatMap(responseToPlay(operation, _)).mkString("\n")}
+            ${responseTypes.map(responseToPlay).mkString("\n")}
             case ${responseEnumName(operation)}.Undocumented(result) => result
           }(defaultExecutionContext)
       }
