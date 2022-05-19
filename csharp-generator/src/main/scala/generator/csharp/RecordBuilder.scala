3f38c7598f0a26db71cd26b44beeef564019488d
Michael Bryzek <mike@flow.io>
2022-05-19 17:50:25 -0400

    update

---
+++ csharp-generator/src/main/scala/generator/csharp/RecordBuilder.scala
@@ -0,0 +1,34 @@
+package generator.csharp
+
+case class RecordField(name: String, `type`: String, required: Boolean)
+
+case class RecordBuilder(
+  name: Option[String] = None,
+  fields: Seq[RecordField] = Nil
+) {
+  def withName(name: String): RecordBuilder = {
+    this.copy(
+      name = Some(name)
+    )
+  }
+
+  def withField(field: RecordField): RecordBuilder = {
+    this.copy(
+      fields = fields ++ Seq(field)
+    )
+  }
+
+  def build: String = {
+    val n = name.getOrElse {
+      sys.error("Missing class name")
+    }
+    Seq(
+      s"public record $n (",
+      fields.map { f =>
+        s"${f.`type`} ${f.name}"
+      }.mkString(",\n").trim.indent(2).stripTrailing(),
+      ");",
+    ).mkString("\n")
+  }
+
+}
--- csharp-generator/src/test/scala/generator/csharp/CSharpGeneratorSpec.scala
+++ csharp-generator/src/test/scala/generator/csharp/CSharpGeneratorSpec.scala
@@ -2,6 +2,7 @@ package generator.csharp
 
 import helpers.{ServiceHelpers, TestHelpers}
 import io.apibuilder.generator.v0.models.InvocationForm
+import io.apibuilder.spec.v0.models.Service
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.must.Matchers
 
@@ -10,9 +11,9 @@ class CSharpGeneratorSpec extends AnyFunSpec with Matchers
   with TestHelpers
 {
 
-  private[this] def makeInvocationForm(): InvocationForm = {
+  private[this] def makeInvocationForm(service: Service = makeService()): InvocationForm = {
     InvocationForm(
-      service = makeService(),
+      service = service,
       attributes = Nil,
       userAgent = None,
       importedServices = None
@@ -25,6 +26,25 @@ class CSharpGeneratorSpec extends AnyFunSpec with Matchers
         makeInvocationForm()
       )
     }
+  }
+
+  it("generate mdoels") {
+    rightOrErrors {
+      CSharpGenerator.invoke(
+        makeInvocationForm(
+          service = makeService(
+            models = Seq(makeModel(
+              name = "user",
+              fields = Seq(makeField("id", `type` = "string"))
+            ))
+          )
+        )
+      )
+    }.head.contents mustBe """
+      |public record User (
+      |  string Id
+      |);
+      |""".stripMargin.trim
   }
 
 }
