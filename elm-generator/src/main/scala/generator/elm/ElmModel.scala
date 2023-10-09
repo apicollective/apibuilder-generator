8ab2121bddbca1372287c75abad638e9bbc95b07
Michael Bryzek <mbryzek@alum.mit.edu>
2023-10-08 21:16:38 -0400

    wip

--- elm-generator/src/main/scala/generator/elm/ElmModel.scala
+++ elm-generator/src/main/scala/generator/elm/ElmModel.scala
@@ -3,11 +3,13 @@ package generator.elm
 import io.apibuilder.spec.v0.models.Model
 
 case class ElmModel(imports: Imports) {
+  private[this] val elmJson = ElmJson(imports)
+
   def generate(model: Model): String = {
     Seq(
       genTypeAlias(model),
-      //genEncoder(model),
-      //genDecoder(model)
+      genEncoder(model),
+      genDecoder(model)
     ).mkString("\n\n")
   }
 
@@ -27,4 +29,36 @@ case class ElmModel(imports: Imports) {
     ).mkString("\n")
   }
 
+
+  /*
+  memberStatusEncoder : MemberStatus -> Encode.Value
+  memberStatusEncoder type_ =
+      Encode.string (memberStatusToString type_)
+   */
+  private[this] def genEncoder(m: Model): String = {
+    imports.addAs("Json.Encode", "Encode")
+    elmJson.encoder(m.name) {
+      Seq(
+        "Encode.object",
+        "[",
+        m.fields.map { f =>
+
+        }.mkString("\n").indent(4),
+        "]"
+      ).mkString("\n").indent(4)
+    }
+  }
+
+  /*
+  memberStatusDecoder : Decoder MemberStatus
+  memberStatusDecoder =
+      Decode.map memberStatusFromString string
+   */
+  private[this] def genDecoder(m: Model): String = {
+    elmJson.decoder(m.name) {
+      m.fields.map { f =>
+
+      }.mkString("\n").indent(4)
+    }
+  }
 }
